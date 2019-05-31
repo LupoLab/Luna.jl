84fc60596d7baf5699e964a259d31dba429d5a75
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2019-05-31 18:58:31 +0100

    major refactor to functional code

--- src/Luna.jl
+++ src/Luna.jl
@@ -4,43 +4,26 @@ import NumericalIntegration
 import Logging
 import Printf: @sprintf
 include("Maths.jl")
-include("RK45.jl")
 include("PhysData.jl")
+include("Grid.jl")
+include("RK45.jl")
 include("Capillary.jl")
-include("Config.jl")
 include("Nonlinear.jl")
 include("Ionisation.jl")
 
-function make_linop(ω, refλ, cap::Configuration.Capillary, medium::Configuration.StaticFill)
-    βfun(ω) = Capillary.β(cap.radius, ω, gas=medium.gas, pressure=medium.pressure)
-    αfun(ω) = Capillary.α(cap.radius, ω)
-    return make_linop(ω, βfun, αfun, refλ)
-end
 
-function make_linop(ω, refλ, pcf::Configuration.HCPCF, medium::Configuration.StaticFill)
-    α = log(10)/10 * pcf.dB_per_m
-    βfun(ω) = Capillary.β(pcf.radius, ω, gas=medium.gas, pressure=medium.pressure)
-    αfun(ω) = α
-    return make_linop(ω, βfun, αfun, refλ)
-end
-
-function make_linop(ω, βfun, αfun, refλ)
-    ω0 = 2π*PhysData.c/refλ
-    idx0 = (ω .== 0)
-    β = zero(ω)
-    α = zero(ω)
-    β[.~idx0] .= .-βfun(ω[.~idx0])
+function make_linop(grid, βfun, αfun)
+    ω0 = 2π*PhysData.c/grid.referenceλ
+    idx0 = (grid.ω .== 0)
+    β = zero(grid.ω)
+    α = zero(grid.ω)
+    β[.~idx0] .= .-βfun(grid.ω[.~idx0])
     β[idx0] .= 0
     β1 = -Maths.derivative(βfun, ω0, 1)
-    α[.~idx0] .= αfun(ω[.~idx0])
+    α[.~idx0] .= αfun(grid.ω[.~idx0])
     α[idx0] .= maximum(α[.~idx0])
 
-    return @. im*(β-β1*ω) - α/2
-end
-
-function make_Pnl_prefac(ω, cap, medium::Configuration.StaticFill)
-    βfun(ω) = Capillary.β(cap.radius, ω, gas=medium.gas, pressure=medium.pressure)
-    return make_Pnl_prefac(ω, βfun)
+    return @. im*(β-β1*grid.ω) - α/2
 end
 
 function make_Pnl_prefac(ω, βfun)
@@ -53,36 +36,21 @@ function make_Pnl_prefac(ω, βfun)
     return out
 end
 
-function make_density(medium::Configuration.StaticFill)
-    return z -> PhysData.std_dens * medium.pressure
-end
-
-function make_fnl(ω, ωo, to, Eω, Et, ωwindow, twindow, conf)
-    cropidx = length(ω)
-    tsamples = Int((length(ωo)-1)*2)
-    Eωo = zeros(ComplexF64, length(ωo))
-    Eto = zeros(Float64, length(to))
+function make_fnl(grid, βfun, densityfun, Pnl_prefac, responses)
+    cropidx = length(grid.ω)
+    tsamples = Int((length(grid.ωo)-1)*2)
+    Eωo = zeros(ComplexF64, length(grid.ωo))
+    Eto = zeros(Float64, length(grid.to))
     FT = FFTW.plan_rfft(zeros(tsamples))
     IFT = FFTW.plan_irfft(Eωo, tsamples)
-    
-    χ3 = PhysData.χ3_gas(conf.medium.gas)
-    kerr! = Nonlinear.make_kerr!(χ3)
 
-    ionpot = PhysData.ionisation_potential(conf.medium.gas)
-    ionrate = Ionisation.ionrate_fun!_ADK(ionpot)
-    plasma! = Nonlinear.make_plasma!(to, ωo, Eto, ionrate, ionpot)
-    responses = (kerr!, plasma!)
-
-    dens = make_density(conf.medium)
-
-    scalefac = (length(ωo)-1)/(length(ω)-1)
+    scalefac = (length(grid.ωo)-1)/(length(grid.ω)-1)
 
     Pt = zeros(Float64, tsamples)
     Pω = similar(Eωo)
     Et = similar(Pt)
-    prefac = make_Pnl_prefac(ω, conf.geometry, conf.medium)
-    fnl! = let Pt=Pt, Et=Et, FT=FT, IFT=IFT, responses=responses, Pω=Pω, prefac=prefac,
-                scalefac=scalefac, Eωo=Eωo, cropidx=cropidx, ωwindow=ωwindow, twindow=twindow
+    fnl! = let Pt=Pt, Et=Et, FT=FT, IFT=IFT, responses=responses, Pω=Pω, prefac=Pnl_prefac,
+                scalefac=scalefac, Eωo=Eωo, cropidx=cropidx, ωwindow=grid.ωwin, twindow=grid.towin
         function fnl!(out, Eω, z)
             fill!(Pt, 0)
             fill!(Eωo, 0)
@@ -94,110 +62,48 @@ function make_fnl(ω, ωo, to, Eω, Et, ωwindow, twindow, conf)
             end
             @. Pt *= twindow
             Pω .= (FT*Pt)
-            out .= ωwindow.*dens(z).*prefac.*Pω[1:cropidx]./scalefac
+            out .= ωwindow.*densityfun(z).*prefac.*Pω[1:cropidx]./scalefac
         end
     end
-    return fnl!, prefac
+    return fnl!
 end
 
-function make_grid(grid::Configuration.Grid)
-    return make_grid(grid.λ_lims, grid.trange, grid.δt, grid.apod_width)
-end
-
-function make_grid(λ_lims, trange, δt, apod_width)
-    f_lims = PhysData.c./λ_lims
-    Logging.@info @sprintf("Freq limits %.2f - %.2f PHz", f_lims[2]*1e-15, f_lims[1]*1e-15)
-    δto = min(1/(6*maximum(f_lims)), δt) # 6x maximum freq, or user-defined if finer
-    samples = 2^(ceil(Int, log2(trange/δto))) # samples for fine grid (power of 2)
-    trange_even = δto*samples # keep frequency window fixed, expand time window as necessary
-    Logging.@info @sprintf("Samples needed: %.2f, samples: %d, δt = %.2f as",
-                            trange/δto, samples, δto*1e18)
-    δωo = 2π/trange_even # frequency spacing for fine grid
-    # Make fine grid
-    Nto = collect(range(0, length=samples))
-    to = @. (Nto-samples/2)*δto # center on 0
-    Nωo = collect(range(0, length=Int(samples/2 +1)))
-    ωo = Nωo*δωo
-
-    ωmax = 2π*maximum(f_lims)
-    cropidx = findfirst(x -> x>ωmax+4*apod_width, ωo)
-    cropidx = 2^(ceil(Int, log2(cropidx))) + 1
-    ω = ωo[1:cropidx]
-    δt = π/maximum(ω)
-    tsamples = (cropidx-1)*2
-    Nt = collect(range(0, length=tsamples))
-    t = @. (Nt-tsamples/2)*δt
-
-    # Make apodisation window
-    ωmin = 2π*minimum(f_lims)
-    # ω_left = ωmin/2
-    # width_left = ωmin/16
-    # ω_right = 2π*maximum(f_lims)+2*apod_width
-    # width_right = apod_width
-    # window = Maths.errfun_window(ω, ω_left, ω_right, width_left, width_right)
-    ωwindow = Maths.planck_taper(ω, 0, ωmin, ωmax, ωmax+3*ωmin) 
-
-    # twindow = Maths.errfun_window(to, minimum(t)+50e-15, maximum(t)-50e-15, 10e-15)
-    twindow = Maths.planck_taper(t, minimum(t), -trange/2, trange/2, maximum(t))
-    towindow = Maths.planck_taper(to, minimum(to), -trange/2, trange/2, maximum(to))
-
-    @assert δt/δto ≈ length(to)/length(t)
-    @assert δt/δto ≈ maximum(ωo)/maximum(ω)
-
-    Logging.@info @sprintf("Grid: samples %d / %d, ωmax %.2e / %.2e",
-                           length(t), length(to), maximum(ω), maximum(ωo))
-    return t, ω, to, ωo, ωwindow, twindow, towindow
-end
-
-function make_init(t, inputs::NTuple{N, T}) where N where T<:Configuration.Input
-    out = fill(0.0 + 0.0im, Int(length(t)/2+1))
+function make_init(grid, inputs, energyfun)
+    out = fill(0.0 + 0.0im, length(grid.ω))
     for input in inputs
-        out .+= make_init(t, input)
+        out .+= scaled_input(grid, input, energyfun)
     end
     return out
 end
 
-function make_init(t, input::Configuration.GaussInput)
-    It = Maths.gauss(t, fwhm=input.duration)
-    Aeff = Capillary.Aeff(13e-6) #!!!! HARDCODED
-    energy = abs(NumericalIntegration.integrate(t, It, NumericalIntegration.SimpsonEven()))
-    energy *= PhysData.c*PhysData.ε_0*Aeff/2
-    It .*= input.energy/energy
-
-    ω0 = 2π*PhysData.c/input.wavelength
-    Et = @. sqrt(It)*cos(ω0*t)
-
-    return FFTW.rfft(Et)
+function scaled_input(grid, input, energyfun)
+    Et = input.func(grid.t)
+    energy = energyfun(grid.t, Et, input.m, input.n)
+    Et_sc = sqrt(input.energy)/sqrt(energy) .* Et
+    return FFTW.rfft(Et_sc)
 end
 
-import PyPlot: pygui, plt
-function run(config)
-    pygui(true)
-    t, ω, to, ωo, window, twindow, towindow = make_grid(config.grid)
 
-    Eω = make_init(t, config.input)
-    Et = FFTW.irfft(Eω, length(t))
+function run(grid,
+             βfun, αfun, energyfun, densityfun, inputs, responses)
 
-    linop = make_linop(ω, config.grid.referenceλ, config.geometry, config.medium)
-    fnl!, prefac = make_fnl(ω, ωo, to, Eω, Et, window, towindow, config)
+    Eω = make_init(grid, inputs, energyfun)
+    Et = FFTW.irfft(Eω, length(grid.t))
 
-    f! = let linop=linop, fnl! = fnl!
-        function f!(out, Eω, z)
-            fnl!(out, Eω, z)
-            out .+= linop.*Eω
-            # out .= linop.*Eω
-        end
-    end
+    Pnl_prefac = make_Pnl_prefac(grid.ω, βfun)
+
+    linop = make_linop(grid, βfun, αfun)
+    fnl! = make_fnl(grid, βfun, densityfun, Pnl_prefac, responses)
 
     z = 0
     dz = 1e-3
-    zmax = config.geometry.length
+    zmax = grid.zmax
     saveN = 201
 
     FT = FFTW.plan_rfft(Et)
-    IFT = FFTW.plan_irfft(Eω, length(t))
+    IFT = FFTW.plan_irfft(Eω, length(grid.t))
 
-    window! = let window=window, twindow=twindow, FT=FT, IFT=IFT, Et=Et
+    window! = let window=grid.ωwin, twindow=grid.twin, FT=FT, IFT=IFT, Et=Et
         function window!(Eω)
             Eω .*= window
             Et .= IFT*Eω
@@ -209,9 +115,9 @@ function run(config)
     zout, Eout, steps = RK45.solve_precon(
         fnl!, linop, Eω, z, dz, zmax, saveN, stepfun=window!)
 
-    Etout = FFTW.irfft(Eout, length(t), 1)
+    Etout = FFTW.irfft(Eout, length(grid.t), 1)
 
-    return ω, t, zout, Eout, Etout, window, twindow, prefac
+    return zout, Eout, Etout
 end
 
 """
@@ -219,7 +125,7 @@ Functions/callables needed
 β(ω, m, n)
 α(ω, m, n) what about α(x, y)?
 energy(t, Et, m, n)
-Et0(t) shape only, input given as list of tuples: (Et0fun, energy, m, n)
+Et0(t) shape only, input given as list of _named_ tuples: (func, energy, m, n)
 Pnl(t, Et)
 density(z) possibly density(x, z, y) or density(ρ, θ, z)?
 """
