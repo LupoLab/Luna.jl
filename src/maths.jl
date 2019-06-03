364049184b95d3eef549a48138e14d04858b49e3
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2019-06-03 11:37:06 +0100

    multi-dim cumtrapz

--- src/maths.jl
+++ src/maths.jl
@@ -9,74 +9,80 @@ function derivative(f, x, order::Integer)
     elseif order == 1
         return ForwardDiff.derivative(f, x)
     else
-        return derivative(x -> ForwardDiff.derivative(f, x), x, order-1)
+        return derivative(x->ForwardDiff.derivative(f, x), x, order - 1)
     end
 end
 
-function gauss(x, σ; x0=0, power=2)
-    return @. exp(-1//2*((x-x0)/σ)^power)
+function gauss(x, σ; x0 = 0, power = 2)
+    return @. exp(-1 // 2 * ((x - x0) / σ)^power)
 end
 
-function gauss(x; x0=0, power=2, fwhm)
-    σ = fwhm/(2*(2*log(2))^(1/power))
-    return gauss(x, σ, x0=x0, power=power)
+function gauss(x; x0 = 0, power = 2, fwhm)
+    σ = fwhm / (2 * (2 * log(2))^(1 / power))
+    return gauss(x, σ, x0 = x0, power = power)
 end
 
-function moment(x::Array{T, 1}, y::Array{T, 1}, n=1) where T
+function moment(x::Array{T,1}, y::Array{T,1}, n = 1) where T
     if length(x) ≠ length(y)
         throw(DomainError(x, "x and y must have same length"))
     end
-    return sum(x.^n .* y)/sum(y)
+    return sum(x.^n .* y) / sum(y)
 end
 
-function moment(x::Array{T, 1}, y, n=1; dim=1) where T
+function moment(x::Array{T,1}, y, n = 1; dim = 1) where T
     if size(y, dim) ≠ length(x)
         throw(DomainError(y, "y must be of same length as x along dim"))
     end
     xshape = ones(Integer, ndims(y))
     xshape[dim] = length(x)
-    return sum(reshape(x, Tuple(xshape)).^n .* y, dims=dim)./sum(y, dims=dim)
+    return sum(reshape(x, Tuple(xshape)).^n .* y, dims = dim) ./ sum(y, dims = dim)
 end
 
-function rms_width(x::Array{T, 1}, y::Array{T, 1}; dim=1) where T
+function rms_width(x::Array{T,1}, y::Array{T,1}; dim = 1) where T
     return sqrt(moment(x, y, 2) - moment(x, y, 1)^2)
 end
 
-function rms_width(x::Array{T, 1}, y; dim=1) where T
-    return sqrt.(moment(x, y, 2, dim=dim) - moment(x, y, 1, dim=dim).^2)
+function rms_width(x::Array{T,1}, y; dim = 1) where T
+    return sqrt.(moment(x, y, 2, dim = dim) - moment(x, y, 1, dim = dim).^2)
 end
 
-function cumtrapz!(y, dx::Number)
-    tmp = y[1]
-    y[1] = 0
-    for i in 2:length(y)
-        tmp2 = y[i]
-        y[i] = y[i-1] + 0.5*dx*(tmp + tmp2)
+function cumtrapz!(y, dx::Number; dim = 1)
+    idxlo = CartesianIndices(size(y)[1:dim - 1])
+    idxhi = CartesianIndices(size(y)[dim + 1:end])
+    tmp = y[idxlo, 1, idxhi]
+    y[idxlo, 1, idxhi] .= 0
+    for i in 2:size(y, dim)
+        tmp2 = y[idxlo, i, idxhi]
+        @. y[idxlo, i, idxhi] = y[idxlo, i - 1, idxhi] + 0.5 * dx * (tmp + tmp2)
         tmp = tmp2
     end
 end
 
-function cumtrapz!(ret, x, y)
-    ret[1] = 0
-    for i in 2 : length(y)
-        ret[i] = ret[i-1] + 0.5*(y[i-1] + y[i])*(x[i] - x[i-1])
+function cumtrapz!(out, x, y; dim = 1)
+    idxlo = CartesianIndices(size(y)[1:dim - 1])
+    idxhi = CartesianIndices(size(y)[dim + 1:end])
+    out[idxlo, 1, idxhi] .= 0
+    for i in 2:size(y, dim)
+        @. out[idxlo, i, idxhi] = (out[idxlo, i - 1, idxhi]
+            + 0.5 * (y[idxlo, i - 1, idxhi] + y[idxlo, i, idxhi])
+                  * (x[i] - x[i - 1]))
     end
 end
 
 function cumtrapz(x, y)
     ret = zero(y)
-    for i in 2 : length(y)
-        ret[i] = ret[i-1] + 0.5*(y[i-1] + y[i])*(x[i] - x[i-1])
+    for i in 2:length(y)
+        ret[i] = ret[i - 1] + 0.5 * (y[i - 1] + y[i]) * (x[i] - x[i - 1])
     end
     return ret
 end
 
 function normbymax(x, dims)
-    return x./maximum(x; dims=dims)
+    return x ./ maximum(x; dims = dims)
 end
 
 function normbymax(x)
-    return x./maximum(x)
+    return x ./ maximum(x)
 end
 
 function log10_norm(x)
@@ -84,112 +90,124 @@ function log10_norm(x)
 end
 
 function log10_norm(x, dims)
-    return log10.(normbymax(x, dims=dims))
+    return log10.(normbymax(x, dims = dims))
 end
 
 function errfun_window(x, xmin, xmax, width)
-    return @. 0.5*(erf((x-xmin)/width) + erfc((x-xmax)/width) - 1)
+    return @. 0.5 * (erf((x - xmin) / width) + erfc((x - xmax) / width) - 1)
 end
 
 function errfun_window(x, xmin, xmax, width_left, width_right)
-    return @. 0.5*(erf((x-xmin)/width_left) + erfc((x-xmax)/width_right) - 1)
+    return @. 0.5 * (erf((x - xmin) / width_left) + erfc((x - xmax) / width_right) - 1)
 end
 
-"Planck taper window as defined in the paper:
+"""
+Planck taper window as defined in the paper:
     xmin: lower limit (window is 0 here)
     xmax: upper limit (window is 0 here)
-    ε: fraction of window width over which to increase from 0 to 1"
+    ε: fraction of window width over which to increase from 0 to 1
+"""
 function planck_taper(x::AbstractArray, xmin, xmax, ε)
     # https://arxiv.org/pdf/1003.2939.pdf eq(7)
-    x0 = (xmax+xmin)/2
+    x0 = (xmax + xmin) / 2
     xc = x .- x0
-    X = (xmax-xmin)
-    x1  = -X/2
-    x2 = -X/2*(1-2ε)
-    x3 = X/2*(1-2ε)
-    x4 = X/2
+    X = (xmax - xmin)
+    x1  = -X / 2
+    x2 = -X / 2 * (1 - 2ε)
+    x3 = X / 2 * (1 - 2ε)
+    x4 = X / 2
     return planck(xc, x1, x2, x3, x4)
 end
 
-"Planck taper window, but finding the taper width by defining 4 points:
+"""
+Planck taper window, but finding the taper width by defining 4 points:
 The window increases from 0 to 1 between left0 and left1, and then drops again
-to 0 between right1 and right0"
+to 0 between right1 and right0
+"""
 function planck_taper(x::AbstractArray, left0, left1, right1, right0)
-    x0 = (right0 + left0)/2
+    x0 = (right0 + left0) / 2
     xc = x .- x0
     X = right0 - left0
-    εleft = abs(left1-left0)/X
-    εright = abs(right0-right1)/X
-    x1  = -X/2
-    x2 = -X/2*(1-2εleft)
-    x3 = X/2*(1-2εright)
-    x4 = X/2
+    εleft = abs(left1 - left0) / X
+    εright = abs(right0 - right1) / X
+    x1  = -X / 2
+    x2 = -X / 2 * (1 - 2εleft)
+    x3 = X / 2 * (1 - 2εright)
+    x4 = X / 2
     return _taper(xc, x1, x2, x3, x4)
 end
 
-"Planck taper helper function, common to both versions of planck_taper"
+"""
+Planck taper helper function, common to both versions of planck_taper
+"""
 function _taper(xc, x1, x2, x3, x4)
     idcs12 = x1 .< xc .< x2
     idcs23 = x2 .<= xc .<= x3
     idcs34 = x3 .< xc .< x4
-    z12 = @. (x2-x1)/(xc[idcs12]-x1) + (x2-x1)/(xc[idcs12]-x2)
-    z34 = @. (x3-x4)/(xc[idcs34]-x3) + (x3-x4)/(xc[idcs34]-x4)
+    z12 = @. (x2 - x1) / (xc[idcs12] - x1) + (x2 - x1) / (xc[idcs12] - x2)
+    z34 = @. (x3 - x4) / (xc[idcs34] - x3) + (x3 - x4) / (xc[idcs34] - x4)
     out = zero(xc)
-    @. out[idcs12] = 1/(1+exp(z12))
+    @. out[idcs12] = 1 / (1 + exp(z12))
     @. out[idcs23] = 1
-    @. out[idcs34] = 1/(1+exp(z34))
+    @. out[idcs34] = 1 / (1 + exp(z34))
     return out
 end
 
-"Hypergaussian window"
-function hypergauss_window(x, xmin, xmax, power=10)
-    fw = xmax-xmin
-    x0 = (xmax+xmin)/2
-    return gauss(x, x0=x0, fwhm=fw, power=power)
+"""
+Hypergaussian window
+"""
+function hypergauss_window(x, xmin, xmax, power = 10)
+    fw = xmax - xmin
+    x0 = (xmax + xmin) / 2
+    return gauss(x, x0 = x0, fwhm = fw, power = power)
 end
 
-"Hilbert transform - find analytic signal from real signal"
-function hilbert(x::Array{T, N}; dim=1) where T<:Real where N
+"""
+Hilbert transform - find analytic signal from real signal
+"""
+function hilbert(x::Array{T,N}; dim = 1) where T <: Real where N
     xf = FFTW.fftshift(FFTW.fft(x, dim), dim)
-    idxlo = CartesianIndices(size(xf)[1:dim-1])
-    idxhi = CartesianIndices(size(xf)[dim+1:end])
-    xf[idxlo, 1:ceil(Int, size(xf, dim)/2), idxhi] .= 0
+    idxlo = CartesianIndices(size(xf)[1:dim - 1])
+    idxhi = CartesianIndices(size(xf)[dim + 1:end])
+    xf[idxlo, 1:ceil(Int, size(xf, dim) / 2), idxhi] .= 0
     return 2 .* FFTW.ifft(FFTW.ifftshift(xf, dim), dim)
 end
 
-"Oversample (smooth) an array by 0-padding in the frequency domain"
-function oversample(t, x::Array{T, N}; factor::Integer=4, dim=1) where T<:Real where N
+"""
+Oversample (smooth) an array by 0-padding in the frequency domain
+"""
+function oversample(t, x::Array{T,N}; factor::Integer = 4, dim = 1) where T <: Real where N
     if factor == 1
         return t, x
     end
     xf = FFTW.rfft(x, dim)
 
     len = size(xf, dim)
-    newlen_t = factor*length(t)
+    newlen_t = factor * length(t)
     if iseven(newlen_t)
-        newlen_ω = Int(newlen_t/2 + 1)
+        newlen_ω = Int(newlen_t / 2 + 1)
     else
-        newlen_ω = Int((newlen_t+1)/2)
+        newlen_ω = Int((newlen_t + 1) / 2)
     end
-    δt = t[2]-t[1]
-    δto = δt/factor
-    Nto = collect(0:newlen_t-1)
-    to = t[1] .+ Nto.*δto
+    δt = t[2] - t[1]
+    δto = δt / factor
+    Nto = collect(0:newlen_t - 1)
+    to = t[1] .+ Nto .* δto
 
     shape = collect(size(xf))
     shape[dim] = newlen_ω
     xfo = zeros(eltype(xf), Tuple(shape))
-    idxlo = CartesianIndices(size(xfo)[1:dim-1])
-    idxhi = CartesianIndices(size(xfo)[dim+1:end])
-    xfo[idxlo, 1:len, idxhi] .= factor.*xf
+    idxlo = CartesianIndices(size(xfo)[1:dim - 1])
+    idxhi = CartesianIndices(size(xfo)[dim + 1:end])
+    xfo[idxlo, 1:len, idxhi] .= factor .* xf
     return to, FFTW.irfft(xfo, newlen_t, dim)
 end
 
-function aitken_accelerate(f, x0; n0=0, rtol=1e-6, maxiter=10000)
+function aitken_accelerate(f, x0; n0 = 0, rtol = 1e-6, maxiter = 10000)
     n = n0
     x0 = f(x0, n)
-    x1 = f(x0, n+1)
-    x2 = f(x1, n+2)
+    x1 = f(x0, n + 1)
+    x2 = f(x1, n + 2)
     Ax = aitken(x0, x1, x2)
     success = false
     while ~success && n < maxiter
@@ -197,10 +215,10 @@ function aitken_accelerate(f, x0; n0=0, rtol=1e-6, maxiter=10000)
         Axprev = Ax
         x0 = x1
         x1 = x2
-        x2 = f(x2, n+2)
+        x2 = f(x2, n + 2)
         Ax = aitken(x0, x1, x2)
 
-        if 2*abs(Ax - Axprev)/abs(Ax + Axprev) < rtol
+        if 2 * abs(Ax - Axprev) / abs(Ax + Axprev) < rtol
             success = true
         end
     end
@@ -209,17 +227,17 @@ end
 
 function aitken(x0, x1, x2)
     den = (x0 - x1) - (x1 - x2)
-    return x0 - (x1 - x0)^2/den
+    return x0 - (x1 - x0)^2 / den
 end
 
-function converge_series(f, x0; n0=0, rtol=1e-6, maxiter=10000)
+function converge_series(f, x0; n0 = 0, rtol = 1e-6, maxiter = 10000)
     n = n0
     x1 = x0
     success = false
     while ~success && n < maxiter
         x1 = f(x0, n)
 
-        if 2*abs(x1 - x0)/abs(x1 + x0) < rtol
+        if 2 * abs(x1 - x0) / abs(x1 + x0) < rtol
             success = true
         end
 
