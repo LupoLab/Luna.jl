66d6d3bdf925c889845c0aec870d8c37056f3317
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2019-08-19 13:32:59 +0100

    transform, integrals working

--- src/QDHT.jl
+++ src/QDHT.jl
@@ -1,7 +1,7 @@
 module Hankel
 import FunctionZeros: besselj_zero
 import SpecialFunctions: besselj
-import LinearAlgebra: mul!, ldiv!
+import LinearAlgebra: mul!, ldiv!, dot
 import Base: *, \
 
 function dot!(out, M, V; dim=1)
@@ -20,39 +20,69 @@ function _dot!(out, M, V, idxlo, idxhi)
     end
 end
 
-#Switch this to type with *, \ methods
+function dimdot(v, A; dim=1)
+    dims = collect(size(A))
+    dims[dim] = 1
+    out = Array{eltype(A)}(undef, Tuple(dims))
+    dimdot!(out, v, A; dim=1)
+    return out
+end
+
+function dimdot(v, A::Vector; dim=1)
+    return dot(v, A)
+end
+
+function dimdot!(out, v, A; dim=1)
+    idxlo = CartesianIndices(size(A)[1:dim-1])
+    idxhi = CartesianIndices(size(A)[dim+1:end])
+    _dimdot!(out, v, A, idxlo, idxhi)
+end
+
+function _dimdot!(out, v, A, idxlo, idxhi)
+    for lo in idxlo
+        for hi in idxhi
+            out[lo, 1, hi] = dot(v, view(A, lo, :, hi))
+        end
+    end
+end
+
 mutable struct QDHT
     N::Int64 # Number of samples
-    C::Array{Float64, 2} # Transform matrix
+    T::Array{Float64, 2} # Transform matrix
     K::Float64 # Highest spatial frequency
     k::Vector{Float64} # Spatial frequency grid
     R::Float64 # Aperture size (largest real-space coordinate)
     r::Vector{Float64} # Real-space grid
+    scaleR::Vector{Float64}
+    scaleK::Vector{Float64}
     dim::Int64 # Dimension along which to transform
 end
 
 function QDHT(R, N; dim=1)
-    roots = besselj_zero.(0, range(1, stop=N+1))
-    S = roots[end]
-    roots = roots[1:end-1]
-    J1sq = besselj.(1, roots).^2
+    roots = besselj_zero.(0, 1:N)
+    S = besselj_zero(0, N+1)
     r = roots .* R/S # real-space vector
     K = S/R # Highest spatial frequency
     k = roots .* K/S # Spatial frequency vector
     # Transform matrix
-    C = 2/S * besselj.(0, (roots * roots')./S)./J1sq
-    QDHT(N, C, K, k, R, r, dim)
+    J1 = abs.(besselj.(1, roots))
+    J1sq = J1 .* J1
+    T = 2/S * besselj.(0, (roots * roots')./S)./J1sq'
+
+    scaleR = 2*(R/S)^2 ./ J1sq
+    scaleK = 2*(K/S)^2 ./ J1sq
+    QDHT(N, T, K, k, R, r, scaleR, scaleK, dim)
 end
 
 "Forward transform"
 function mul!(Y, Q::QDHT, A)
-    dot!(Y, Q.C, A, dim=Q.dim)
+    dot!(Y, Q.T, A, dim=Q.dim)
     Y .*= Q.R/Q.K
 end
 
 "Inverse transform"
 function ldiv!(Y, Q::QDHT, A)
-    dot!(Y, Q.C, A, dim=Q.dim)
+    dot!(Y, Q.T, A, dim=Q.dim)
     Y .*= Q.K/Q.R
 end
 
@@ -64,6 +94,15 @@ end
 function \(Q::QDHT, A)
     out = similar(A)
     ldiv!(out, Q, A)
+end
+
+"Radial integral"
+function integrateR(A, Q)
+    return dimdot(Q.scaleR, A)
+end
+
+function integrateK(A, Q)
+    return dimdot(Q.scaleK, A)
 end
 
 end
--- test/test_qdht.jl
+++ test/test_qdht.jl
@@ -3,38 +3,39 @@ import Luna: Hankel
 import LinearAlgebra: diagm
 import FunctionZeros: besselj_zero
 import SpecialFunctions: besselj
-import Cubature: hquadrature
+import NumericalIntegration: integrate, Trapezoidal
 
 @testset "multiplication" begin
-M = diagm(0 => [1, 2, 3])
-V = 2 .* ones((3, 3, 2))
-out = similar(V)
-Hankel.dot!(out, M, V)
-@test all(out[1, :, :] .== 2)
-@test all(out[2, :, :] .== 4)
-@test all(out[3, :, :] .== 6)
-Hankel.dot!(out, M, V, dim=2)
-@test all(out[:, 1, :] .== 2)
-@test all(out[:, 2, :] .== 4)
-@test all(out[:, 3, :] .== 6)
-@test_throws DomainError Hankel.dot!(out, M, V, dim=3)
+    M = diagm(0 => [1, 2, 3])
+    V = 2 .* ones((3, 3, 2))
+    out = similar(V)
+    Hankel.dot!(out, M, V)
+    @test all(out[1, :, :] .== 2)
+    @test all(out[2, :, :] .== 4)
+    @test all(out[3, :, :] .== 6)
+    Hankel.dot!(out, M, V, dim=2)
+    @test all(out[:, 1, :] .== 2)
+    @test all(out[:, 2, :] .== 4)
+    @test all(out[:, 3, :] .== 6)
+    @test_throws DomainError Hankel.dot!(out, M, V, dim=3)
 end
 
-q = Hankel.QDHT(1, 256)
-u11 = besselj_zero(0, 1)
-f(r) = besselj.(0, u11.*r)
-v = f(q.r)
-vv = copy(v)
-for _ = 1:1000
-    tmp = q * v
-    vv = q \ v
+@testset "transform" begin
+    q = Hankel.QDHT(1, 128)
+    @test all(isreal.(q.T))
+    a = 50
+    f(r) = exp(-1//2 * a^2 * r^2)
+    fk(k) = 1/a^2 * exp(-k^2/(2*a^2))
+    v = f.(q.r)
+    for _ = 1:1000
+        tmp = q * v
+        global vv = q \ tmp
+    end
+    @test all(v ≈ vv)
+    vk = q * v
+    vka = fk.(q.k)
+    @test all(vka ≈ vk)
+    Er = Hankel.integrateR(v.^2, q)
+    Ek = Hankel.integrateK(vk.^2, q)
+    @test Er ≈ Ek
 end
-@test all(v ≈ vv)
-vk = q \ v
-vki = zero(q.k)
-for (idx, ki) in enumerate(q.k)
-    vki[idx] = hquadrature(r -> r*f(r)*besselj(0, ki*r), 0, q.R)[1]
-    println(idx)
-end
-# import PyPlot: pygui, plt
-# pygui(true)
