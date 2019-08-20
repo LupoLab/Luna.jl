c21f7f6cf0e21a84fcfe5181142ff74a2494c400
John Travers <jtravs@gmail.com>
2019-08-20 22:08:38 +0100

    transfer code from multimode

--- src/AbstractModes.jl
+++ src/AbstractModes.jl
@@ -1,9 +1,88 @@
 module AbstractModes
+import Roots: fzero
+import Luna: Maths
+import Luna.PhysData: c, ε_0
 
 abstract type AbstractMode end
 
+"Maximum radius valid for this mode"
+function maxR(m::M) where {M <: AbstractMode}
+    error("abstract method called")
+end
 
+function β(m::M, ω) where {M <: AbstractMode}
+    error("abstract method called")
+end
 
+function α(m::M, ω) where {M <: AbstractMode}
+    error("abstract method called")
+end
 
+function dispersion_func(m::M, order) where {M <: AbstractMode}
+    βn(ω) = Maths.derivative(ω -> β(m, ω), ω, order)
+    return βn
+end
+
+function zdw(m::M) where {M <: AbstractMode}
+    ω0 = fzero(dispersion_func(m, 2), 1e14, 2e16) # TODO magic numbers
+    return 2π*c/ω0
+end
+
+"Create function that returns (r,θ) -> (Ex, Ey)"
+function field(m::M) where {M <: AbstractMode}
+    error("abstract method called")
+end
+
+"Get mode normalization constant"
+function N(m::M) where {M <: AbstractMode}
+    f = field(m)
+    function Nfunc(x)
+        r = x[1]
+        θ = x[2]
+        E = f(r, θ)
+        r*sqrt(ε_0/μ_0)*dot(E, E)
+    end
+    val, err = hcubature(Nfunc, (0.0, 0.0), (maxR(m), 2π))
+    0.5*abs(val)
+end
+
+"Create function that returns normalised (r,θ) -> |E|"
+function absE(m::M) where {M <: AbstractMode}
+    func = let sN = sqrt(N(m)), f = field(m)
+        function func(r,θ)
+            norm(f(r,θ) ./ sN)
+        end
+    end
+end
+
+"Create function that returns normalised (r,θ) -> (Ex, Ey)"
+function Exy(m) where {M <: AbstractMode}
+    func = let sN = sqrt(N(m)), f = field(m)
+        function func(r,θ)
+            f(r,θ) ./ sN
+        end
+    end
+end
+
+"Get effective area of mode"
+function Aeff(m) where {M <: AbstractMode}
+    em = absE(m)
+    function Aefft(x)
+        r = x[1]
+        θ = x[2]
+        e = em(r, θ)
+        r*e^2
+    end
+    val, err = hcubature(Aefft, (0.0, 0.0), (maxR(m), 2π))
+    ret = val^2
+    function Aeffb(x)
+        r = x[1]
+        θ = x[2]
+        e = em(r, θ)
+        r*e^4
+    end
+    val, err = hcubature(Aeffb, (0.0, 0.0), (maxR(m), 2π))
+    return ret / val
+end
 
 end
--- src/Capillary.jl
+++ src/Capillary.jl
@@ -8,9 +8,22 @@ import SpecialFunctions: besselj
 import Luna: Maths
 import Luna.PhysData: c, ε_0, χ1, ref_index
 
+function getunm(n, m, kind)
+    if (kind == :TE) || (kind == :TM)
+        if (n != 0) || (m != 1)
+            error("n=0, m=1 for TE or TM modes")
+        end
+        return besselj_zero(1, 1)
+    elseif kind == :HE
+        return besselj_zero(n-1, m)
+    else
+        error("kind must be :TE, :TM or :HE")
+    end
+end
+
 function β(a, ω; gas::Symbol=:He, pressure=0, n=1, m=1)
     χ = pressure .* χ1(gas, 2π*c./ω)
-    unm = besselj_zero(n-1, m)
+    unm = getunm(n, m, :HE)
     return @. ω/c*(1 + χ/2 - c^2*unm^2/(2*ω^2*a^2))
 end
 
@@ -18,43 +31,62 @@ function β(a; λ, gas::Symbol=:He, pressure=0, n=1, m=1)
     return β(a, 2π*c./λ, gas=gas, pressure=pressure, n=n, m=m)
 end
 
-function α(a, ω; n=1, m=1)
-    unm = besselj_zero(n-1, m)
+function α(a, ω; n=1, m=1, kind=:HE)
+    unm = getunm(n, m, kind)
     ν = ref_index(:SiO2, 2π*c./ω)
-    return @. 2*(c^2 * unm^2)/(a^3 * ω^2) * (ν^2 + 1)/(2*real(sqrt(Complex(ν^2-1))))
+    if kind == :HE
+        vp = @. (ν^2 + 1)/(2*real(sqrt(Complex(ν^2-1))))
+    elseif kind == :TE
+        vp = @. 1/(real(sqrt(Complex(ν^2-1))))
+    elseif kind == :TM
+        vp = @. ν^2/(real(sqrt(Complex(ν^2-1))))
+    else
+        error("kind must be :TE, :TM or :HE")
+    end
+    return @. 2*(c^2 * unm^2)/(a^3 * ω^2) * vp
 end
 
-function α(a, ω::AbstractArray; n=1, m=1)
-    unm = besselj_zero(n-1, m)
+# TODO: why do we repeat this?
+function α(a, ω::AbstractArray; n=1, m=1, kind=:HE)
+    unm = getunm(n, m, kind)
     ν = ref_index(:SiO2, 2π*c./ω)
     ν[ω .< 3e14] .= 1.4
-    ret = @. 2*(c^2 * unm^2)/(a^3 * ω^2) * (ν^2 + 1)/(2*real(sqrt(Complex(ν^2-1))))
+    if kind == :HE
+        vp = @. (ν^2 + 1)/(2*real(sqrt(Complex(ν^2-1))))
+    elseif kind == :TE
+        vp = @. 1/(real(sqrt(Complex(ν^2-1))))
+    elseif kind == :TM
+        vp = @. ν^2/(real(sqrt(Complex(ν^2-1))))
+    else
+        error("kind must be :TE, :TM or :HE")
+    end
+    ret = @. 2*(c^2 * unm^2)/(a^3 * ω^2) * vp
     ret[isinf.(ret)] .= 0 # TODO FIND OUT WHY THIS IS NEEDED
     return ret
 end
 
-function α(a; λ, n=1, m=1)
-    return α(a, 2π*c./λ, n=n, m=m)
+function α(a; λ, n=1, m=1, kind=:HE)
+    return α(a, 2π*c./λ, n=n, m=m, kind=:HE)
 end
 
-function losslength(a, ω; n=1, m=1)
-    return 1 ./ α(a, ω, n=n, m=m)
+function losslength(a, ω; n=1, m=1, kind=:HE)
+    return 1 ./ α(a, ω, n=n, m=m, kind=:HE)
 end
 
-function losslength(a; λ, n=1, m=1)
-    return 1 ./ α(a, n=n, m=m, λ=λ)
+function losslength(a; λ, n=1, m=1, kind=:HE)
+    return 1 ./ α(a, n=n, m=m, λ=λ, kind=:HE)
 end
 
-function transmission(a, L; λ, n=1, m=1)
-    return @. exp(-α(a, λ=λ, n=n, m=m)*L)
+function transmission(a, L; λ, n=1, m=1, kind=:HE)
+    return @. exp(-α(a, λ=λ, n=n, m=m, kind=:HE)*L)
 end
 
-function dB_per_m(a, ω; n=1, m=1)
-    return 10/log(10).*α(a, ω, n=n, m=m)
+function dB_per_m(a, ω; n=1, m=1, kind=:HE)
+    return 10/log(10).*α(a, ω, n=n, m=m, kind=:HE)
 end
 
-function dB_per_m(a; n=1, m=1, λ)
-    return return 10/log(10) .* α(a, n=n, m=m, λ=λ)
+function dB_per_m(a; n=1, m=1, λ=λ, kind=:HE)
+    return return 10/log(10) .* α(a, n=n, m=m, λ=λ, kind=:HE)
 end
 
 function dispersion_func(order, a; gas::Symbol=:He, pressure=0, n=1, m=1)
@@ -75,24 +107,69 @@ function zdw(a; gas::Symbol, pressure, n=1, m=1)
     return 2π*c/ω0
 end
 
-function Aeff(a, n=1, m=1)
-    nJ = n > 0 ? n-1 : 2 # n=0 -> TE/TM modes so n-1 should be 1
-    unm = besselj_zero(nJ, m)
-    return 2π*a^2*(hquadrature(r-> r*besselj(nJ, r*unm)^2, 0, 1)[1]^2
-    / hquadrature(r-> r*besselj(nJ, r*unm)^4, 0, 1)[1])
+"Create function that returns (r,θ) -> (Ex, Ey)"
+function field(a, n, m, kind; ϕ=0.0)
+    unm = getunm(n, m, kind)
+    if kind == :HE
+        return (r, θ) -> besselj(n-1, r*unm/a) .* SVector(cos(θ)*sin(n*(θ + ϕ)) - sin(θ)*cos(n*(θ + ϕ)),
+                                                          sin(θ)*sin(n*(θ + ϕ)) + cos(θ)*cos(n*(θ + ϕ)))
+    elseif kind == :TE
+        return (r, θ) -> besselj(1, r*unm/a) .* SVector(-sin(θ), cos(θ))
+    elseif kind == :TM
+        return (r, θ) -> besselj(1, r*unm/a) .* SVector(cos(θ), sin(θ))
+    end
 end
 
-function modefield(a, n=1, m=1)
-    nJ = n > 0 ? n-1 : 2 # n=0 -> TE/TM modes so n-1 should be 1
-    unm = besselj_zero(nJ, m)
-    norm = c*ε_0*π*hquadrature(r-> r*besselj(nJ, r*unm/a)^2, 0, a)[1]
-    return r -> @. besselj(nJ, r*unm/a)/sqrt(norm)
+"Get mode normalization constant"
+function N(a, n, m, kind; ϕ=0.0)
+    f = field(a, n, m, kind, ϕ=ϕ)
+    function Nfunc(x)
+        r = x[1]
+        θ = x[2]
+        E = f(r, θ)
+        r*sqrt(ε_0/μ_0)*dot(E, E)
+    end
+    val, err = hcubature(Nfunc, (0.0, 0.0), (a, 2π))
+    0.5*abs(val)
 end
 
-function mode_average(a, n=1, m=1)
-    mode = modefield(a, n, m)
-    integrand = r -> r.*mode(r).^4
-    return 2π*hquadrature(integrand, 0, a)[1]
+"Create function that returns normalised (r,θ) -> |E|"
+function emagh(a, n, m, kind; ϕ=0.0)
+    func = let sN = sqrt(N(a, n, m, kind, ϕ=ϕ)), f = field(a, n, m, kind, ϕ=ϕ)
+        function func(r,θ)
+            norm(f(r,θ) ./ sN)
+        end
+    end
+end
+
+"Create function that returns normalised (r,θ) -> (Ex, Ey)"
+function getExy(a, n, m, kind; ϕ=0.0)
+    func = let sN = sqrt(N(a, n, m, kind, ϕ=ϕ)), f = field(a, n, m, kind, ϕ=ϕ)
+        function func(r,θ)
+            f(r,θ) ./ sN
+        end
+    end
+end
+
+"Get effective area of mode"
+function Aeff(a, n, m, kind; ϕ=0.0)
+    em = emagh(a, n, m, kind, ϕ=ϕ)
+    function Aefft(x)
+        r = x[1]
+        θ = x[2]
+        e = em(r, θ)
+        r*e^2
+    end
+    val, err = hcubature(Aefft, (0.0, 0.0), (a, 2π))
+    ret = val^2
+    function Aeffb(x)
+        r = x[1]
+        θ = x[2]
+        e = em(r, θ)
+        r*e^4
+    end
+    val, err = hcubature(Aeffb, (0.0, 0.0), (a, 2π))
+    return ret / val
 end
 
 end
