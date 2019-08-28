a63b1e05b67b0efff14d6689780a4e30398649de
Mohammed Sabbah <ms246@hw.ac.uk>
2019-08-28 19:05:50 +0100

    correct time format

--- src/RK45.jl
+++ src/RK45.jl
@@ -45,10 +45,11 @@ function solve(s, tmax; stepfun=donothing!, output=false, outputN=201,
         steps += 1
         if Dates.value(Dates.now()-tic) > 1000*status_period
             speed = s.tn/(Dates.value(Dates.now()-start)/1000)
-            eta = (tmax-s.tn)/(speed)
-            
+            eta_in_s = (tmax-s.tn)/(speed)
+            eta_in_ms = Dates.Millisecond(ceil(eta_in_s*1000))
+            etad = Dates.DateTime(Dates.UTInstant(eta_in_ms))
             Logging.@info @sprintf("Progress: %.2f %%, ETA: %s, stepsize %.2e, err %.2f, repeated %d",
-                s.tn/tmax*100, hms(eta), s.dt, s.err, repeated_tot)
+                s.tn/tmax*100, Dates.format(etad, "HH:MM:SS"), s.dt, s.err, repeated_tot)
             tic = Dates.now()
         end
         if ok
@@ -315,12 +316,6 @@ function weaknorm(yerr, y, yn, rtol, atol)
     end
     errwt = max(max(sy, syn), atol)
     return syerr/rtol/errwt
-end
-
-function hms(dt)
-    (h,r) = divrem(dt,60*60)
-    (m,r) = divrem(r, 60)
-    string(Int(h),":",Int(m),":",Int64(round(r)))
 end
 
 function donothing!(y, z, dz, interpolant)
