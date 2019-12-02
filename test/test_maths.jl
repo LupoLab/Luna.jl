438ba3a0cda4a1bab55d0578c245e7d59901ca79
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2019-12-02 14:41:09 +0000

    fix fast indexing for CSpline

--- test/test_maths.jl
+++ test/test_maths.jl
@@ -104,13 +104,19 @@ end
     @test all(pl[8 .< x] .== 0)
 end
 
-@testset "Spline" begin
+# @testset "Spline" begin
     x = range(0.0, 2π, length=100)
     y = sin.(x)
     spl = Maths.CSpline(x, y)
+    fslow(x0) = x0 <= spl.x[1] ? 2 :
+                x0 >= spl.x[end] ? length(spl.x) :
+                findfirst(x -> x>x0, spl.x)
     @test all(abs.(spl.(x) .- y) .< 5e-18)
     x2 = range(0.0, 2π, length=300)
+    idcs = spl.ifun.(x2)
+    idcs_slow = fslow.(x2)
+    @test all(idcs .== idcs_slow)
     @test maximum(spl.(x2) - sin.(x2)) < 5e-8
     @test abs(Maths.derivative(spl, 1.3, 1) - cos(1.3)) < 1.7e-7
     @test maximum(cos.(x2) - Maths.derivative.(spl, x2, 1)) < 2.1e-6
-end
+# end
