671f9fd801406243825b2cb1b97d84ab98069eca
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-01-20 17:52:44 +0000

    comparison to Heyl et al in test_freespace.jl

--- test/test_freespace.jl
+++ test/test_freespace.jl
@@ -1,5 +1,5 @@
 import Luna
-import Luna: Grid, Maths, PhysData, Nonlinear, Ionisation, NonlinearRHS, Output, Stats, LinearOps, Hankel
+import Luna: Grid, Maths, PhysData, Nonlinear, Ionisation, NonlinearRHS, Output, Stats, LinearOps, Hankel, Plotting
 import Logging
 import FFTW
 import NumericalIntegration: integrate, SimpsonEven
@@ -8,27 +8,40 @@ Logging.disable_logging(Logging.BelowMinLevel)
 import PyPlot:pygui, plt
 
 gas = :Ar
-pres = 4
+pres = 1.2
 
-τ = 30e-15
+τ = 20e-15
 λ0 = 800e-9
 
-w0 = 2e-3
-energy = 1.5e-3
-L = 2
+w0 = 40e-6
+energy = 2e-9
+L = 0.6
 
-R = 6e-3
-N = 256
+R = 4e-3
+N = 1024
 
 grid = Grid.RealGrid(L, 800e-9, (400e-9, 2000e-9), 0.2e-12)
 q = Hankel.QDHT(R, N, dim=2)
 
 energyfun = NonlinearRHS.energy_radial(q)
 
+function prop(E, z)
+    Eω = FFTW.rfft(E, 1)
+    Eωk = q * Eω
+    kzsq = @. (grid.ω/PhysData.c)^2 - (q.k^2)'
+    kzsq[kzsq .< 0] .= 0
+    kz = sqrt.(kzsq)
+    @. Eωk *= exp(-1im * z * (kz - grid.ω/PhysData.c))
+    Eω = q \ Eωk
+    E = FFTW.irfft(Eω, length(grid.t), 1)
+    return E
+end
+
 function gausspulse(t)
     It = Maths.gauss(t, fwhm=τ) .* Maths.gauss(q.r, w0/2)'
     ω0 = 2π*PhysData.c/λ0
     Et = @. sqrt(It)*cos(ω0*t)
+    return prop(Et, -0.3)
 end
 
 dens0 = PhysData.density(gas, pres)
@@ -84,10 +97,18 @@ end
 
 ω0idx = argmin(abs.(grid.ω .- 2π*PhysData.c/λ0))
 
+zr = π*w0^2/λ0
+points = L/2 .+ [-15, 3, 21].*zr
+idcs = [argmin(abs.(zout .- point)) for point in points]
+
+Epoints = [Hankel.symmetric(Et[:, :, idxi], q) for idxi in idcs]
+rsym = Hankel.Rsymmetric(q);
+
 pygui(true)
 Iλ0 = Iωr[ω0idx, :, :]
 λ0 = 2π*PhysData.c/grid.ω[ω0idx]
-w1 = w0*sqrt(1+(L*λ0/(π*w0^2))^2)
+w1 = w0*sqrt(1+(L/2*λ0/(π*w0^2))^2)
+# w1 = w0
 Iλ0_analytic = Maths.gauss(q.r, w1/2)*(w0/w1)^2 # analytical solution (in paraxial approx)
 plt.figure()
 plt.plot(q.r*1e3, Maths.normbymax(Iλ0[:, end]))
@@ -111,8 +132,10 @@ plt.title("I(r, t=0)")
 
 plt.figure()
 plt.pcolormesh(zout*1e2, q.r*1e3, Ir)
+# plt.pcolormesh(zout*1e2, q.r*1e3, log10.(Maths.normbymax(Ir)))
 plt.colorbar()
-plt.ylim(0, 4)
+plt.ylim(0, R*1e3)
+# plt.clim(-6, 0)
 plt.xlabel("z (m)")
 plt.ylabel("r (m)")
 plt.title("\$\\int I(r, \\omega) d\\omega\$")
@@ -125,8 +148,17 @@ plt.xlabel("z (m)")
 plt.ylabel("f (PHz)")
 plt.title("I(r=0, ω)")
 
-
 plt.figure()
 plt.plot(zout.*1e2, energy.*1e6)
 plt.xlabel("Distance [cm]")
 plt.ylabel("Energy [μJ]")
+
+jw = Plotting.cmap_white("jet")
+fig = plt.figure()
+fig.set_size_inches(12, 4)
+for ii in 1:3
+    plt.subplot(1, 3, ii)
+    plt.pcolormesh(grid.t*1e15, rsym*1e3, abs2.(Epoints[ii]'), cmap=jw)
+    plt.xlim(-42, 42)
+    plt.ylim(-1.8, 1.8)
+end
