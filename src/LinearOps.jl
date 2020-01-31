dfa990b5fb1d2af24cdbc6c5ef94ae115dc93cfd
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-01-31 09:04:51 +0000

    gradient for envelope

--- src/LinearOps.jl
+++ src/LinearOps.jl
@@ -14,7 +14,9 @@ function make_const_linop(grid::Grid.RealGrid, βfun, αfun, frame_vel)
 end
 
 function make_const_linop(grid::Grid.EnvGrid, βfun, αfun, frame_vel, β0ref)
-    β = βfun(grid.ω, 0)
+    β = similar(grid.ω)
+    βfun(β, grid.ω, 0)
+    β .*= -1
     α = αfun(grid.ω, 0)
     β1 = 1/frame_vel(0)
     return -im.*(β .- β1.*(grid.ω .- grid.ω0) .- β0ref) .- α./2
@@ -30,10 +32,12 @@ function make_const_linop(grid::Grid.EnvGrid, mode::Modes.AbstractMode, λ0; thg
     βconst = zero(grid.ω)
     βconst[grid.sidx] = Modes.β.(mode, grid.ω[grid.sidx])
     βconst[.!grid.sidx] .= 1
-    βfun(ω, z) = βconst
+    function βfun!(out, ω, z)
+        out .= βconst
+    end
     frame_vel(z) = 1/β1const
     αfun(ω, z) = Modes.α.(mode, ω)
-    make_const_linop(grid, βfun, αfun, frame_vel, β0const), βfun, frame_vel, αfun
+    make_const_linop(grid, βfun!, αfun, frame_vel, β0const), βfun!, frame_vel, αfun
 end
 
 function make_const_linop(grid::Grid.RealGrid, mode::Modes.AbstractMode, λ0)
@@ -84,13 +88,33 @@ end
 
 function make_linop(grid::Grid.RealGrid, mode::Modes.AbstractMode, λ0)
     function linop!(out, z)
-        out .= -im.*grid.ω./PhysData.c.*Modes.neff.(mode, grid.ω, z=z)
-        out[1] = 1
+        out[2:end] .= -im.*grid.ω[2:end]./PhysData.c.*Modes.neff.(mode, grid.ω[2:end], z=z)
         out .-= -im.*grid.ω.*Modes.dispersion(mode, 1, change(λ0), z=z)
+        out[1] = 1
     end
     function βfun!(out, ω, z)
-        out .= Modes.β.(mode, ω, z=z)
+        out[2:end] .= Modes.β.(mode, ω[2:end], z=z)
         out[1] = 1
+    end
+    return linop!, βfun!
+end
+
+function make_linop(grid::Grid.EnvGrid, mode::Modes.AbstractMode, λ0; thg=false)
+    function linop!(out, z)
+        fill!(out, 1.0)
+        out[grid.sidx] .= -im.*(
+            grid.ω[grid.sidx]./PhysData.c.*Modes.neff.(mode, grid.ω[grid.sidx], z=z)
+            )
+        out[grid.sidx] .-= -im.*(
+            (grid.ω[grid.sidx] .- grid.ω0).*Modes.dispersion(mode, 1, change(λ0), z=z)
+            )
+        if !thg
+            out .-= -im.*Modes.β(mode, change(λ0))
+        end
+    end
+    function βfun!(out, ω, z)
+        fill!(out, 1.0)
+        out[grid.sidx] .= Modes.β.(mode, ω[grid.sidx], z=z)
     end
     return linop!, βfun!
 end
@@ -105,6 +129,24 @@ function make_linop(grid::Grid.RealGrid, modes, λ0; ref_mode=1)
                 .- grid.ω[2:end] .* β1
                 )
             out[1, i] = 1
+        end
+    end
+end
+
+function make_linop(grid::Grid.EnvGrid, modes, λ0; ref_mode=1, thg=false)
+    function linop!(out, z)
+        β1 = Modes.dispersion(modes[ref_mode], 1, change(λ0), z=z)
+        βref = Modes.β(modes[ref_mode], change(λ0))
+        nmodes = length(modes)
+        fill!(out, 1.0)
+        for i = 1:nmodes
+            out[grid.sidx, i] .= -im.*(
+                grid.ω[grid.sidx]./PhysData.c.*Modes.neff.(modes[i], grid.ω[grid.sidx], z=z)
+                .- (grid.ω[grid.sidx] .- grid.ω0) .* β1
+                )
+            if !thg
+                out[grid.sidx, i] .-= -im.*βref
+            end
         end
     end
 end
--- test/test_gradient_env_modal.jl
+++ test/test_gradient_env_modal.jl
@@ -1,5 +1,5 @@
 import Luna
-import Luna: Grid, Maths, Capillary, PhysData, Nonlinear, Ionisation, NonlinearRHS, RK45, Stats, Output, LinearOps
+import Luna: Grid, Maths, Capillary, PhysData, Nonlinear, Ionisation, NonlinearRHS, RK45, Stats, Output, LinearOps, Modes
 import Logging
 import FFTW
 import NumericalIntegration: integrate, SimpsonEven
@@ -15,11 +15,17 @@ pres = 5
 τ = 30e-15
 λ0 = 800e-9
 
-modes = (Capillary.MarcatilliMode(a, gas, pres, n=1, m=1, kind=:HE, ϕ=0.0, loss=false),
-         Capillary.MarcatilliMode(a, gas, pres, n=1, m=2, kind=:HE, ϕ=0.0, loss=false))
+L = 15e-2
+
+coren, densityfun = Capillary.gradient(gas, L, pres, pres);
+
+modes = (
+    Capillary.MarcatilliMode(a, coren, n=1, m=1, kind=:HE, ϕ=0.0, loss=false),
+    Capillary.MarcatilliMode(a, coren, n=1, m=2, kind=:HE, ϕ=0.0, loss=false)
+)
 nmodes = length(modes)
 
-grid = Grid.EnvGrid(15e-2, 800e-9, (160e-9, 3000e-9), 1e-12)
+grid = Grid.EnvGrid(L, 800e-9, (160e-9, 3000e-9), 1e-12)
 
 energyfun = NonlinearRHS.energy_env_modal()
 normfun = NonlinearRHS.norm_modal(grid.ω)
@@ -30,23 +36,17 @@ function gausspulse(t)
     Et = @. sqrt(It)
 end
 
-dens(z) = PhysData.std_dens * pres
-
-ionpot = PhysData.ionisation_potential(gas)
-ionrate = Ionisation.ionrate_fun!_ADK(ionpot)
-
 responses = (Nonlinear.Kerr_env(PhysData.γ3_gas(gas)),)
-             #Nonlinear.PlasmaCumtrapz(grid.to, grid.to, ionrate, ionpot))
 
 in1 = (func=gausspulse, energy=1e-6)
 inputs = ((1,(in1,)),)
 
-Eω, transform, FT = Luna.setup(grid, energyfun, dens, normfun, responses, inputs,
-                              modes, :Ey; full=false)
+Eω, transform, FT = Luna.setup(grid, energyfun, densityfun, normfun, responses, inputs,
+                               modes, :Ey; full=false)
 
 statsfun = Stats.collect_stats((Stats.ω0(grid), ))
 output = Output.MemoryOutput(0, grid.zmax, 201, (length(grid.ω),length(modes)), statsfun)
-linop = LinearOps.make_const_linop(grid, modes, λ0)
+linop = LinearOps.make_linop(grid, modes, λ0)
 
 Luna.run(Eω, grid, linop, transform, FT, output)
 
