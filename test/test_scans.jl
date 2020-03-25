46e6a100a9f52a5608177c26e8e6c78f24cd6da2
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-03-25 10:20:59 +0000

    more tests

--- test/test_scans.jl
+++ test/test_scans.jl
@@ -1,5 +1,5 @@
 import Test: @test, @testset, @test_throws
-import Luna: Scans
+import Luna: Scans, Utils
 
 @testset "Chunking" begin
     function contains_all_unique(chunks, x)
@@ -19,4 +19,38 @@ import Luna: Scans
         end
     end
     @test all(pass)
+end
+
+@testset "Scanning" begin
+for _ in eachindex(ARGS)
+    pop!(ARGS)
+end
+push!(ARGS, "--range", "1:4")
+Scans.@scaninit "scantest"
+@test __SCAN__.mode == :range
+@test __SCAN__.name == "scantest"
+@test !__SCAN__.parallel
+
+@scanvar dummy = range(1, length=10)
+@test __SCAN__.vars[:dummy] == dummy
+@test __SCAN__.values[dummy] == dummy
+
+Scans.@scan begin
+    x = $dummy
+    @test x == dummy[__SCANIDX__]
+end
+
+for _ in eachindex(ARGS)
+    pop!(ARGS)
+end
+push!(ARGS, "--batch", "1,3")
+Scans.@scaninit "scantest"
+@test __SCAN__.mode == :batch
+@scanvar dummy = range(1, length=10)
+@test __SCAN__.batch == (1, 3)
+@test __SCAN__.idcs == [1, 4, 7, 10]
+Scans.@scan begin
+    x = $dummy
+    @test x == dummy[__SCANIDX__]
+end
 end
