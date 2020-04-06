5b3ef7dbfea58a9a3aa1185a313bf477d5211fff
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-04-06 11:03:33 +0100

    scaled response function

--- src/Nonlinear.jl
+++ src/Nonlinear.jl
@@ -3,6 +3,24 @@ import Luna.PhysData: ε_0, e_ratio
 import Luna: Maths
 import FFTW
 
+"""
+    scaled_response(resp, scale, shape)
+
+Scale the response function `resp` by the factor `scale`. `shape` should be `size(Eto)` where
+`Eto` is the oversampled field in time.
+"""
+function scaled_response(resp, scale, shape)
+    buffer = Array{ComplexF64}(undef, shape)
+    let buffer=buffer, resp=resp
+        function resp!(out, E)
+            fill!(buffer, 0)
+            resp(buffer, E)
+            @. out += scale*buffer
+        end
+    end
+end
+
+
 function KerrScalar(out, E, fac)
     @. out += fac*E^3
 end
