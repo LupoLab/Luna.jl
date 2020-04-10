3b1750dcf4a9596034f06e2ac3415e4641df3a95
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-04-10 13:54:42 +0100

    add some docs structure

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,17 @@ github_ref        = get(ENV, "GITHUB_REF",        "")
 cfg = Documenter.GitHubActions(github_repository, github_event_name, github_ref)
 
 makedocs(
-    sitename="Luna Documentation",
+    sitename = "Luna Documentation",
+    authors = "Christian Brahms and John C. Travers",
+    pages = Any[
+        "Home" => "index.md",
+        "The numerical model" => [
+            "General description" => "model.md",
+            "Modal decompositions" => "modal_decompositions.md",
+            "Nonlinear responses" => "nonlinear_responses.md"
+        ],
+        "Scans" => "scans.md"
+    ],
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     )
