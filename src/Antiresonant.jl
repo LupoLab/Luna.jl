5defb1785cb7fff9a53704dd73290a5f580d0564
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-04-15 18:02:23 +0100

    docs

--- src/Antiresonant.jl
+++ src/Antiresonant.jl
@@ -10,16 +10,31 @@ struct ZeisbergerMode{mT<:Capillary.MarcatilliMode} <: AbstractMode
     wallthickness::Float64
 end
 
+"""
+    ZeisbergerMode(args...; wallthickness, kwargs...)
+
+Create a capillary-like mode with the effective index given by eq. (15) in [1]. All arguments
+and keyword arguments other than `wallthickness` (mandatory kwarg) are passed on to the
+constructor of a [`MarcatilliMode`](@ref).
+
+[1] Zeisberger, M., Schmidt, M.A. Analytic model for the complex effective index of the
+leaky modes of tube-type anti-resonant hollow core fibers. Sci Rep 7, 11761 (2017).
+https://doi.org/10.1038/s41598-017-12234-5
+"""
 function ZeisbergerMode(args...; wallthickness, kwargs...)
     return ZeisbergerMode(Capillary.MarcatilliMode(args...; kwargs...), wallthickness)
 end
 
 ZeisbergerMode(m::Capillary.MarcatilliMode; wallthickness) = ZeisbergerMode(m, wallthickness)
 
+# Effective index is given by eq (15) in [1]
 neff(m::ZeisbergerMode, ω; z=0) = _neff(m.m, ω, m.wallthickness; z=z)
 
-"get the effective complex refractive index for AR-PCF. 
- We follow https://www.nature.com/articles/s41598-017-12234-5"
+# All other mode properties are identical to a MarcatilliMode
+for fun in (:Aeff, :field, :N, :dimlimits)
+    @eval ($fun)(m::ZeisbergerMode, args...; kwargs...) = ($fun)(m.m, args...; kwargs...)
+end
+
 function _neff(m::Capillary.MarcatilliMode, ω, wallthickness; z=0)
     nco = m.coren(ω, z=z)
     ncl = m.cladn(ω, z=z)
@@ -56,10 +71,6 @@ function _neff(m::Capillary.MarcatilliMode, ω, wallthickness; z=0)
         neff = nco*(1 - A*σ^2 - B*σ^3 - C*σ^4 + 1im*D*σ^4)
     end
     return neff
-end
-
-for fun in (:Aeff, :field, :N, :dimlimits)
-    @eval ($fun)(m::ZeisbergerMode, args...; kwargs...) = ($fun)(m.m, args...; kwargs...)
 end
 
 end
