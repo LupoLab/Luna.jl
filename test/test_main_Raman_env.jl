e7d0eb71666834f0fc950406bd93d5250477ead5
John Travers <jtravs@gmail.com>
2020-04-17 13:11:50 +0100

    fix Raman examples

--- test/test_main_Raman_env.jl
+++ test/test_main_Raman_env.jl
@@ -1,5 +1,4 @@
-import Luna
-import Luna: Grid, Maths, Capillary, PhysData, Nonlinear, Ionisation, NonlinearRHS, Output, Stats, LinearOps, Modes, Raman
+using Luna
 import Logging
 import FFTW
 Logging.disable_logging(Logging.BelowMinLevel)
@@ -17,7 +16,7 @@ grid = Grid.EnvGrid(200e-2, 800e-9, (180e-9, 3000e-9), 4e-12)
 m = Capillary.MarcatilliMode(a, gas, pres, loss=false)
 aeff(z) = Modes.Aeff(m, z=z)
 
-energyfun = NonlinearRHS.energy_modal()
+energyfun, energyfunω = NonlinearRHS.energy_modal(grid)
 
 function gausspulse(t)
     It = Maths.gauss(t, fwhm=τ)
@@ -39,7 +38,13 @@ inputs = (in1, )
 
 Eω, transform, FT = Luna.setup(grid, energyfun, densityfun, normfun, responses, inputs, aeff)
 
-statsfun = Stats.collect_stats((Stats.ω0(grid), ))
+statsfun = Stats.collect_stats(grid, Eω,
+                               Stats.ω0(grid),
+                               Stats.energy(grid, energyfunω),
+                               Stats.energy_λ(grid, energyfunω, (150e-9, 300e-9), label="RDW"),
+                               Stats.peakpower(grid),
+                               Stats.fwhm_t(grid),
+                               Stats.density(densityfun))
 output = Output.MemoryOutput(0, grid.zmax, 201, (length(grid.ω),), statsfun)
 
 Luna.run(Eω, grid, linop, transform, FT, output)
@@ -48,12 +53,10 @@ Luna.run(Eω, grid, linop, transform, FT, output)
 t = grid.t
 f = FFTW.fftshift(ω, 1)./2π.*1e-15
 
-zout = output.data["z"]
-Eout = output.data["Eω"]
+zout = output["z"]
+Eout = output["Eω"]
 
 Etout = FFTW.ifft(Eout, 1)
-
-
 
 Ilog = log10.(Maths.normbymax(abs2.(Eout)))
 
