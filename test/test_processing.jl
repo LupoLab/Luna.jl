3bf3eee513a3f60fb5572881948f056a168320f5
John Travers <jtravs@gmail.com>
2020-05-14 22:05:28 +0100

    add freq tests

--- test/test_processing.jl
+++ test/test_processing.jl
@@ -115,12 +115,19 @@ Eω = (Maths.gauss.(grid.ω, fwhm=2π*PhysData.c/800e-9^2*2e-9, x0=PhysData.wlfr
 ωp, Eωp = Processing.getEω(grid, Eω)
 λg, Pλ = Processing.getIω(ωp, Eωp, :λ, resolution=10e-9, specrange=(170e-9, 2900e-9))
 @test isapprox(Fields.energyfuncs(grid)[2](Eω) / integrate(λg, Pλ), 1.0, rtol=1e-10)
+Fg, Pf = Processing.getIω(ωp, Eωp, :f, resolution=5e12)
+# TODO why is the frequency domain integral much less exact?
+@test isapprox(Fields.energyfuncs(grid)[2](Eω) / integrate(Fg, Pf), 1.0, rtol=2e-2)
 # test resolution
 Eω = (Maths.gauss.(grid.ω, fwhm=2π*PhysData.c/800e-9^2*0.1e-9, x0=PhysData.wlfreq(grid.referenceλ)))
 ωp, Eωp = Processing.getEω(grid, Eω)
 for res in (2e-9, 10e-9, 20e-9)
     λg, Pλ = Processing.getIω(ωp, Eωp, :λ, resolution=res, specrange=(170e-9, 2900e-9))
     @test isapprox(Maths.fwhm(λg, Pλ), res, rtol=1e-2)
+end
+for res in (1e12, 5e12, 10e12)
+    Fg, Pf = Processing.getIω(ωp, Eωp, :f, resolution=res)
+    @test isapprox(Maths.fwhm(Fg, Pf), res, rtol=1e-2)
 end
 # envelope grid
 grid = Grid.EnvGrid(1.0, 800e-9, (160e-9, 3000e-9), 30e-12)
@@ -131,11 +138,18 @@ Eω = (Maths.gauss.(grid.ω, fwhm=2π*PhysData.c/800e-9^2*2e-9, x0=PhysData.wlfr
 ωp, Eωp = Processing.getEω(grid, Eω)
 λg, Pλ = Processing.getIω(ωp, Eωp, :λ, resolution=10e-9, specrange=(170e-9, 2900e-9))
 @test isapprox(Fields.energyfuncs(grid)[2](Eω) / integrate(λg, Pλ), 1.0, rtol=1e-11)
+Fg, Pf = Processing.getIω(ωp, Eωp, :f, resolution=5e12)
+# TODO why is the frequency domain integral much less exact?
+@test isapprox(Fields.energyfuncs(grid)[2](Eω) / integrate(Fg, Pf), 1.0, rtol=2e-2)
 # test resolution
 Eω = (Maths.gauss.(grid.ω, fwhm=2π*PhysData.c/800e-9^2*0.1e-9, x0=PhysData.wlfreq(grid.referenceλ)))
 ωp, Eωp = Processing.getEω(grid, Eω)
 for res in (2e-9, 10e-9, 20e-9)
     λg, Pλ = Processing.getIω(ωp, Eωp, :λ, resolution=res, specrange=(170e-9, 2900e-9))
     @test isapprox(Maths.fwhm(λg, Pλ), res, rtol=1e-2)
+end
+for res in (1e12, 5e12, 10e12)
+    Fg, Pf = Processing.getIω(ωp, Eωp, :f, resolution=res)
+    @test isapprox(Maths.fwhm(Fg, Pf), res, rtol=1e-2)
 end
 end
