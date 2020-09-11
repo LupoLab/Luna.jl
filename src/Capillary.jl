5d6a5be675581617151c20185f885a575f9f7377
Mohammed Sabbah <ms246@hw.ac.uk>
2020-09-11 11:22:21 +0100

    correct conflict

---
+++ src/Capillary.jl
@@ -0,0 +1,337 @@
+module Capillary
+import FunctionZeros: besselj_zero
+import SpecialFunctions: besselj
+import StaticArrays: SVector
+import Cubature: hquadrature
+using Reexport
+@reexport using Luna.Modes
+import Luna: Maths, Grid
+import Luna.PhysData: c, ε_0, μ_0, ref_index_fun, roomtemp, densityspline, sellmeier_gas
+import Luna.Modes: AbstractMode, dimlimits, neff, field, Aeff, N
+import Luna.LinearOps: make_linop, conj_clamp, neff_grid, neff_β_grid
+import Luna.PhysData: wlfreq
+import Luna.Utils: subscript
+import Base: show
+
+export MarcatilliMode, dimlimits, neff, field, N, Aeff
+
+struct MarcatilliMode{Ta, Tcore, Tclad, LT} <: AbstractMode
+    a::Ta # core radius callable as function of z only, or fixed core radius if a Number
+    n::Int # azimuthal mode index
+    m::Int # radial mode index
+    kind::Symbol # kind of mode (transverse magnetic/electric or hybrid)
+    unm::Float64 # mth zero of the nth Bessel function of the first kind
+    ϕ::Float64 # overall rotation angle of the mode
+    coren::Tcore # callable, returns (possibly complex) core ref index as function of ω
+    cladn::Tclad # callable, returns (possibly complex) cladding ref index as function of ω
+    model::Symbol # if :full, includes complete influence of complex cladding ref index
+    loss::LT # Val{true}() or Val{false}() - whether to include the loss
+    aeff_intg::Float64 # Pre-calculated integral fraction for effective area
+end
+
+function show(io::IO, m::MarcatilliMode)
+    a = radius_string(m)
+    loss = "loss=" * (m.loss == Val(true) ? "true" : "false")
+    model = "model="*string(m.model)
+    out = "MarcatilliMode{"*join([mode_string(m), a, loss, model], ", ")*"}"
+    print(io, out)
+end
+
+mode_string(m::MarcatilliMode) = string(m.kind)*subscript(m.n)*subscript(m.m)
+radius_string(m::MarcatilliMode{<:Number, Tco, Tcl, LT}) where {Tco, Tcl, LT} = "a=$(m.a)"
+radius_string(m::MarcatilliMode) = "a(z=0)=$(radius(m, 0))"
+
+"""
+    MarcatilliMode(a, n, m, kind, ϕ, coren, cladn; model=:full, loss=true)
+Create a MarcatilliMode.
+# Arguments
+- `a` : Either a `Number` for constant core radius, or a function `a(z)` for variable radius.
+- `n::Int` : Azimuthal mode index (number of nodes in the field along azimuthal angle).
+- `m::Int` : Radial mode index (number of nodes in the field along radial coordinate).
+- `kind::Symbol` : `:TE` for transverse electric, `:TM` for transverse magnetic,
+                   `:HE` for hybrid mode.
+- `ϕ::Float` : Azimuthal offset angle (for linearly polarised modes, this is the angle
+                between the mode polarisation and the `:y` axis)
+- `coren` : Callable `coren(ω; z)` which returns the refractive index of the core
+- `cladn` : Callable `cladn(ω; z)` which returns the refractive index of the cladding
+- `model::Symbol=:full` : If `:full`, use the complete Marcatilli model which takes into
+                          account the dispersive influence of the cladding refractive index.
+                          If `:reduced`, use the simplified model common in the literature
+- `loss::Bool=true` : Whether to include loss.
+"""
+function MarcatilliMode(a, n, m, kind, ϕ, coren, cladn; model=:full, loss=true)
+    # chkzkwarg makes sure that coren and cladn take z as a keyword argument
+    aeff_intg = Aeff_Jintg(n, get_unm(n, m, kind), kind)
+    MarcatilliMode(a, n, m, kind, get_unm(n, m, kind), ϕ,
+                   chkzkwarg(coren), chkzkwarg(cladn),
+                   model, Val(loss), aeff_intg)
+end
+
+"""
+    MarcatilliMode(a, gas, P; kwargs...)
+Create a MarcatilliMode for a capillary with radius `a` which is filled with `gas` to
+pressure `P`.
+"""
+function MarcatilliMode(a, gas, P;
+                        n=1, m=1, kind=:HE, ϕ=0.0, T=roomtemp, model=:full,
+                        clad=:SiO2, loss=true)
+    rfg = ref_index_fun(gas, P, T)
+    rfs = ref_index_fun(clad)
+    coren = (ω; z) -> rfg(wlfreq(ω))
+    cladn = (ω; z) -> rfs(wlfreq(ω))
+    MarcatilliMode(a, n, m, kind, ϕ, coren, cladn, model=model, loss=loss)
+end
+
+"""
+    MarcatilliMode(a, gas, P cladn; kwargs...)
+Create a MarcatilliMode for a capillary made of a cladding material defined by the refractive
+index `cladn(ω; z)` with a core radius `a` which is filled with `gas` to pressure `P`.
+"""
+function MarcatilliMode(a, gas, P, cladn;
+                        n=1, m=1, kind=:HE, ϕ=0.0, T=roomtemp, model=:full, loss=true)
+    rfg = ref_index_fun(gas, P, T)
+    coren = (ω; z) -> rfg(wlfreq(ω))
+    MarcatilliMode(a, n, m, kind, ϕ, coren, cladn, model=model, loss=loss)
+end
+
+"""
+    MarcatilliMode(a, coren; kwargs...)
+Create a MarcatilliMode for a capillary with radius `a` with `z`-dependent gas fill determined
+by `coren(ω; z)`.
+"""
+function MarcatilliMode(a, coren;
+                        n=1, m=1, kind=:HE, ϕ=0.0, model=:full, clad=:SiO2, loss=true)
+    rfs = ref_index_fun(clad)
+    cladn = (ω; z) -> rfs(wlfreq(ω))
+    MarcatilliMode(a, n, m, kind, ϕ, coren, cladn, model=model, loss=loss)
+end
+
+"""
+    neff(m::MarcatilliMode, ω; z=0)
+    
+Calculate the complex effective index of Marcatilli mode with dielectric core and arbitrary
+(metal or dielectric) cladding.
+Adapted from:
+Marcatili, E. & Schmeltzer, R.
+"Hollow metallic and dielectric waveguides for long distance optical transmission and lasers
+(Long distance optical transmission in hollow dielectric and metal circular waveguides,
+examining normal mode propagation)."
+Bell System Technical Journal 43, 1783–1809 (1964).
+"""
+function neff(m::MarcatilliMode, ω; z=0)
+    εcl = m.cladn(ω, z=z)^2
+    εco = m.coren(ω, z=z)^2
+    vn = get_vn(εcl, m.kind)
+    neff(m, ω, εco, vn, radius(m, z))
+end
+
+# Dispatch on loss to make neff type stable
+# m.loss = Val{true}() (returns ComplexF64)
+function neff(m::MarcatilliMode{Ta, Tco, Tcl, Val{true}}, ω, εco, vn, a) where {Ta, Tcl, Tco}
+    if m.model == :full
+        k = ω/c
+        n = sqrt(complex(εco - (m.unm/(k*a))^2*(1 - im*vn/(k*a))^2))
+        return (real(n) < 1e-3) ? (1e-3 + im*clamp(imag(n), 0, Inf)) : n
+    elseif m.model == :reduced
+        return ((1 + (εco - 1)/2 - c^2*m.unm^2/(2*ω^2*a^2))
+                    + im*(c^3*m.unm^2)/(a^3*ω^3)*vn)
+    else
+        error("model must be :full or :reduced")
+    end 
+end
+
+# m.loss = Val{false}() (returns Float64)
+function neff(m::MarcatilliMode{Ta, Tco, Tcl, Val{false}}, ω, εco, vn, a) where {Ta, Tcl, Tco}
+    if m.model == :full
+        k = ω/c
+        n = real(sqrt(εco - (m.unm/(k*a))^2*(1 - im*vn/(k*a))^2))
+        return (n < 1e-3) ? 1e-3 : n
+    elseif m.model == :reduced
+        return real(1 + (εco - 1)/2 - c^2*m.unm^2/(2*ω^2*a^2))
+    else
+        error("model must be :full or :reduced")
+    end 
+end
+
+function neff_wg(m::MarcatilliMode{Ta, Tco, Tcl, Val{true}}, ω; z=0) where {Ta, Tcl, Tco}
+    εcl = m.cladn(ω, z=z)^2
+    vn = get_vn(εcl, m.kind)
+    a = radius(m, z)
+    if m.model == :full
+        k = ω/c
+        return (m.unm/(k*a))^2*(1 - im*vn/(k*a))^2
+    elseif m.model == :reduced
+        return c^2*m.unm^2/(2*ω^2*a^2) + im*(c^3*m.unm^2)/(a^3*ω^3)*vn
+    else
+        error("model must be :full or :reduced")
+    end
+end
+
+function neff_wg(m::MarcatilliMode{Ta, Tco, Tcl, Val{false}}, ω; z=0) where {Ta, Tcl, Tco}
+    εcl = m.cladn(ω, z=z)^2
+    vn = get_vn(εcl, m.kind)
+    a = radius(m, z)
+    if m.model == :full
+        k = ω/c
+        return (m.unm/(k*a))^2*(1 - im*vn/(k*a))^2
+    elseif m.model == :reduced
+        return c^2*m.unm^2/(2*ω^2*a^2)
+    else
+        error("model must be :full or :reduced")
+    end
+end
+
+function neff(m::MarcatilliMode{Ta, Tco, Tcl, Val{true}}, εco, nwg) where {Ta, Tcl, Tco}
+    if m.model == :full
+        return sqrt(complex(εco - nwg))
+    elseif m.model == :reduced
+        return complex((1 + (εco - 1)/2 - nwg))
+    else
+        error("model must be :full or :reduced")
+    end 
+end
+
+function neff(m::MarcatilliMode{Ta, Tco, Tcl, Val{false}}, εco, nwg) where {Ta, Tcl, Tco}
+    if m.model == :full
+        return real(sqrt(complex(εco - nwg)))
+    elseif m.model == :reduced
+        return real((1 + (εco - 1)/2 - nwg))
+    else
+        error("model must be :full or :reduced")
+    end 
+end
+
+function get_vn(εcl, kind)
+    if kind == :HE
+        (εcl + 1)/(2*sqrt(complex(εcl - 1)))
+    elseif kind == :TE
+        1/sqrt(complex(εcl - 1))
+    elseif kind == :TM
+        εcl/sqrt(complex(εcl - 1))
+    else
+        error("kind must be :TE, :TM or :HE")
+    end
+end
+
+function get_unm(n, m, kind)
+    if (kind == :TE) || (kind == :TM)
+        if (n != 0) || (m != 1)
+            error("n=0, m=1 for TE or TM modes")
+        end
+        besselj_zero(1, 1)
+    elseif kind == :HE
+        besselj_zero(n-1, m)
+    else
+        error("kind must be :TE, :TM or :HE")
+    end
+end
+
+radius(m::MarcatilliMode{<:Number, Tco, Tcl, LT}, z) where {Tcl, Tco, LT} = m.a
+radius(m::MarcatilliMode, z) = m.a(z)
+
+dimlimits(m::MarcatilliMode; z=0) = (:polar, (0.0, 0.0), (radius(m, z), 2π))
+
+# we use polar coords, so xs = (r, θ)
+function field(m::MarcatilliMode, xs; z=0)
+    if m.kind == :HE
+        return (besselj(m.n-1, xs[1]*m.unm/radius(m, z)) .* SVector(
+            cos(xs[2])*sin(m.n*(xs[2] + m.ϕ)) - sin(xs[2])*cos(m.n*(xs[2] + m.ϕ)),
+            sin(xs[2])*sin(m.n*(xs[2] + m.ϕ)) + cos(xs[2])*cos(m.n*(xs[2] + m.ϕ))
+            ))
+    elseif m.kind == :TE
+        return besselj(1, xs[1]*m.unm/radius(m, z)) .* SVector(-sin(xs[2]), cos(xs[2]))
+    elseif m.kind == :TM
+        return besselj(1, xs[1]*m.unm/radius(m, z)) .* SVector(cos(xs[2]), sin(xs[2]))
+    end
+end
+
+function N(m::MarcatilliMode; z=0)
+    np1 = (m.kind == :HE) ? m.n : 2
+    π/2 * radius(m, z)^2 * besselj(np1, m.unm)^2 * sqrt(ε_0/μ_0)
+end
+
+function Aeff_Jintg(n, unm, kind)
+    den, err = hquadrature(r -> r*besselj(n-1, unm*r)^4, 0, 1)
+    np1 = (kind == :HE) ? n : 2
+    num = 1/4 * besselj(np1, unm)^4
+    return 2π*num/den
+end
+
+Aeff(m::MarcatilliMode; z=0) = radius(m, z)^2 * m.aeff_intg
+
+
+"Convenience function to create density and core index profiles for
+simple two-point gradient fills."
+function gradient(gas, L, p0, p1)
+    γ = sellmeier_gas(gas)
+    dspl = densityspline(gas, Pmin=p0==p1 ? 0 : min(p0, p1), Pmax=max(p0, p1))
+    p(z) =  z > L ? p1 :
+            z <= 0 ? p0 : 
+            sqrt(p0^2 + z/L*(p1^2 - p0^2))
+    dens(z) = dspl(p(z))
+    coren(ω; z) = sqrt(1 + γ(wlfreq(ω)*1e6)*dens(z))
+    return coren, dens
+end
+
+"Convenience function to create density and core index profiles for
+multi-point gradient fills."
+function gradient(gas, Z, P)
+    γ = sellmeier_gas(gas)
+    ex = extrema(P)
+    dspl = densityspline(gas, Pmin=ex[1]==ex[2] ? 0 : ex[1], Pmax=ex[2])
+    function p(z)
+        if z <= Z[1]
+            return P[1]
+        elseif z >= Z[end]
+            return P[end]
+        else
+            i = findfirst(x -> x < z, Z)
+            return sqrt(P[i]^2 + (z - Z[i])/(Z[i+1] - Z[i])*(P[i+1]^2 - P[i]^2))
+        end
+    end
+    dens(z) = dspl(p(z))
+    coren(ω; z) = sqrt(1 + γ(wlfreq(ω)*1e6)*dens(z))
+    return coren, dens
+end
+
+#= Avoid repeated calculation of the waveguide part of the effective index for modes with
+    constant core radius.
+    This is used by LinearOps.make_linop =# 
+function neff_β_grid(grid,
+                   mode::MarcatilliMode{<:Number, Tco, Tcl, LT} where {Tco, Tcl, LT},
+                   λ0)
+    nwg = complex(zero(grid.ω))
+    sidcs = (1:length(grid.ω))[grid.sidx]
+    for iω in sidcs
+        nwg[iω] = neff_wg(mode, grid.ω[iω]; z=0)
+    end
+    _neff = let nwg=nwg, ω=grid.ω, mode=mode
+        _neff(iω; z) = neff(mode, mode.coren(ω[iω], z=z)^2, nwg[iω])
+    end
+    _β = let nwg=nwg, ω=grid.ω, _neff=_neff
+        _β(iω; z) = ω[iω]/c*real(_neff(iω; z=z))
+    end
+    _neff, _β
+end
+
+# Collection of modes with fixed core radius
+FixedCoreCollection = Union{
+    Tuple{Vararg{MarcatilliMode{<:Number, Tco, Tcl, LT}} where {Tco, Tcl, LT}},
+    AbstractArray{MarcatilliMode{<:Number, Tco, Tcl, LT} where {Tco, Tcl, LT}}
+    }
+
+function neff_grid(grid, modes::FixedCoreCollection, λ0; ref_mode=1)
+    nwg = Array{ComplexF64, 2}(undef, (length(grid.ω), length(modes)))
+    sidcs = (1:length(grid.ω))[grid.sidx]
+    for (i, mi) in enumerate(modes)
+        for iω in sidcs
+            nwg[iω, i] = neff_wg(mi, grid.ω[iω]; z=0)
+        end
+    end
+    _neff = let nwg=nwg, ω=grid.ω, modes=modes
+        _neff(iω, iim; z) = neff(modes[iim], modes[iim].coren(ω[iω], z=z)^2, nwg[iω, iim])
+    end
+    _neff
+end
+
+end
---
+++ src/Luna.jl
@@ -0,0 +1,343 @@
+module Luna
+import FFTW
+import Hankel
+import Logging
+import LinearAlgebra: mul!, ldiv!
+Logging.disable_logging(Logging.BelowMinLevel)
+
+"""
+    HDF5LOCK
+Lock on the HDF5 library for multi-threaded execution.
+"""
+const HDF5LOCK = ReentrantLock()
+
+"""
+    @hlock
+Wait for HDF5LOCK, execute the expression, and release H5DFLOCK.
+!!! warning
+    For thread safety, any call to functions from HDF5.jl needs to be preceeded by @hlock.
+"""
+macro hlock(expr)
+    quote
+        try
+            lock(HDF5LOCK)
+            $(esc(expr))
+        finally
+            unlock(HDF5LOCK)
+        end
+    end
+end
+
+"""
+    Luna.settings
+Dictionary of global settings for `Luna`.
+"""
+settings = Dict{String, Any}("fftw_flag" => FFTW.PATIENT,
+                             "fftw_threads" => 0)
+
+"""
+    set_fftw_mode(mode)
+Set FFTW planning mode for all FFTW transform planning in `Luna`.
+Possible values for `mode` are `:estimate`, `:measure`, `:patient`, and `:exhaustive`.
+The initial value upon loading `Luna` is `:patient`
+# Examples
+```jldoctest
+julia> Luna.set_fftw_mode(:patient)
+0x00000020
+```
+"""
+function set_fftw_mode(mode)
+    s = uppercase(string(mode))
+    flag = getfield(FFTW, Symbol(s))
+    settings["fftw_flag"] = flag
+end
+
+"""
+    set_fftw_threads(nthr)
+Set number of threads to be used by FFTW. If set to `0`, the number of threads used by 
+FFTW is determined automatically (see [`Utils.FFTWthreads()`](@ref))
+"""
+function set_fftw_threads(nthr=0)
+    settings["fftw_threads"] = nthr
+    FFTW.set_num_threads(Utils.FFTWthreads())
+end
+
+function __init__()
+    set_fftw_threads()
+end
+
+include("Utils.jl")
+include("Scans.jl")
+include("Output.jl")
+include("Maths.jl")
+include("PhysData.jl")
+include("Grid.jl")
+include("Fields.jl")
+include("RK45.jl")
+include("Modes.jl")
+include("LinearOps.jl")
+include("Capillary.jl")
+include("Antiresonant.jl")
+include("RectModes.jl")
+include("Nonlinear.jl")
+include("Ionisation.jl")
+include("NonlinearRHS.jl")
+include("Stats.jl")
+include("Polarisation.jl")
+include("Tools.jl")
+include("Processing.jl")
+include("Plotting.jl")
+include("Raman.jl")
+
+export Utils, Scans, Output, Maths, PhysData, Grid, RK45, Modes, Capillary, RectModes,
+       Nonlinear, Ionisation, NonlinearRHS, LinearOps, Stats, Polarisation,
+       Tools, Plotting, Raman, Antiresonant, Fields, Processing
+
+# for a tuple of TimeFields we assume all inputs are for mode 1
+function doinput_sm(grid, inputs::Tuple{Vararg{T} where T <: Fields.TimeField}, FT)
+    out = fill(0.0 + 0.0im, length(grid.ω))
+    for field in inputs
+        out .+= field(grid, FT)
+    end
+    return out
+end
+
+# for a single Fields.TimeField we assume a single input for mode 1
+function doinput_sm(grid, inputs::Fields.TimeField, FT)
+    doinput_sm(grid, (inputs,), FT)
+end
+
+function setup(grid::Grid.RealGrid, densityfun, responses, inputs, βfun!, aeff;
+               norm! = NonlinearRHS.norm_mode_average(grid, βfun!, aeff))
+    Utils.loadFFTwisdom()
+    xo = Array{Float64}(undef, length(grid.to))
+    FTo = FFTW.plan_rfft(xo, 1, flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransModeAvg(grid, FTo, responses, densityfun, norm!, aeff)
+    x = Array{Float64}(undef, length(grid.t))
+    FT = FFTW.plan_rfft(x, 1, flags=settings["fftw_flag"])
+    Eω = doinput_sm(grid, inputs, FT)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eω, transform, FT
+end
+
+function setup(grid::Grid.EnvGrid, densityfun, responses, inputs, βfun!, aeff;
+               norm! = NonlinearRHS.norm_mode_average(grid, βfun!, aeff))
+    Utils.loadFFTwisdom()
+    x = Array{ComplexF64}(undef, length(grid.t))
+    FT = FFTW.plan_fft(x, 1, flags=settings["fftw_flag"])
+    xo = Array{ComplexF64}(undef, length(grid.to))
+    FTo = FFTW.plan_fft(xo, 1, flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransModeAvg(grid, FTo, responses, densityfun, norm!, aeff)
+    Eω = doinput_sm(grid, inputs, FT)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eω, transform, FT
+end
+
+# for a tuple of NamedTuple's with tuple fields we assume all is well
+function doinput_mm!(Eω, grid, inputs::Tuple{Vararg{T} where T <: NamedTuple{<:Any, <:Tuple{Vararg{Any}}}}, FT)
+    for input in inputs
+        out = @view Eω[:,input.mode]
+        for field in input.fields
+            out .+= field(grid, FT)
+        end
+    end
+end
+
+# for a tuple of TimeFields we assume all inputs are for mode 1
+function doinput_mm!(Eω, grid, inputs::Tuple{Vararg{T} where T <: Fields.TimeField}, FT)
+    doinput_mm!(Eω, grid, ((mode=1, fields=inputs),), FT)
+end
+
+# for a single Fields.TimeField we assume a single input for mode 1
+function doinput_mm!(Eω, grid, inputs::Fields.TimeField, FT)
+    doinput_mm!(Eω, grid, ((mode=1, fields=(inputs,)),), FT)
+end
+
+function setup(grid::Grid.RealGrid, densityfun, responses, inputs,
+               modes::Modes.ModeCollection, components;
+               full=false, norm! = NonlinearRHS.norm_modal(grid))
+    ts = Modes.ToSpace(modes, components=components)
+    Utils.loadFFTwisdom()
+    xt = Array{Float64}(undef, length(grid.t))
+    FTt = FFTW.plan_rfft(xt, 1, flags=settings["fftw_flag"])
+    Eω = zeros(ComplexF64, length(grid.ω), length(modes))
+    doinput_mm!(Eω, grid, inputs, FTt)
+    x = Array{Float64}(undef, length(grid.t), length(modes))
+    FT = FFTW.plan_rfft(x, 1, flags=settings["fftw_flag"])
+    xo = Array{Float64}(undef, length(grid.to), ts.npol)
+    FTo = FFTW.plan_rfft(xo, 1, flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransModal(grid, ts, FTo,
+                                 responses, densityfun, norm!,
+                                 rtol=1e-3, atol=0.0, mfcn=300, full=full)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eω, transform, FT
+end
+
+function setup(grid::Grid.EnvGrid, densityfun, responses, inputs,
+               modes::Modes.ModeCollection, components;
+               full=false, norm! = NonlinearRHS.norm_modal(grid))
+    ts = Modes.ToSpace(modes, components=components)
+    Utils.loadFFTwisdom()
+    xt = Array{ComplexF64}(undef, length(grid.t))
+    FTt = FFTW.plan_fft(xt, 1, flags=settings["fftw_flag"])
+    Eω = zeros(ComplexF64, length(grid.ω), length(modes))
+    doinput_mm!(Eω, grid, inputs, FTt)
+    x = Array{ComplexF64}(undef, length(grid.t), length(modes))
+    FT = FFTW.plan_fft(x, 1, flags=settings["fftw_flag"])
+    xo = Array{ComplexF64}(undef, length(grid.to), ts.npol)
+    FTo = FFTW.plan_fft(xo, 1, flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransModal(grid, ts, FTo,
+                                 responses, densityfun, norm!,
+                                 rtol=1e-3, atol=0.0, mfcn=300, full=full)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eω, transform, FT
+end
+
+function doinputs_fs!(Eωk, grid, spacegrid::Union{Hankel.QDHT,Grid.FreeGrid}, FT,
+                   inputs::Tuple{Vararg{T} where T <: Fields.SpatioTemporalField})
+    for field in inputs
+        Eωk .+= field(grid, spacegrid, FT)
+    end
+end
+
+function doinputs_fs!(Eωk, grid, spacegrid::Union{Hankel.QDHT,Grid.FreeGrid}, FT,
+                   inputs::Fields.SpatioTemporalField)
+    doinputs_fs!(Eωk, grid, spacegrid, FT, (inputs,))
+end
+
+function setup(grid::Grid.RealGrid, q::Hankel.QDHT,
+               densityfun, normfun, responses, inputs)
+    Utils.loadFFTwisdom()
+    xt = zeros(Float64, length(grid.t), length(q.r))
+    FT = FFTW.plan_rfft(xt, 1, flags=settings["fftw_flag"])
+    Eω = zeros(ComplexF64, length(grid.ω), length(q.k))
+    Eωk = q * Eω
+    doinputs_fs!(Eωk, grid, q, FT, inputs)
+    xo = Array{Float64}(undef, length(grid.to), length(q.r))
+    FTo = FFTW.plan_rfft(xo, 1, flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransRadial(grid, q, FTo, responses, densityfun, normfun)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eωk, transform, FT
+end
+
+function setup(grid::Grid.EnvGrid, q::Hankel.QDHT,
+               densityfun, normfun, responses, inputs)
+    Utils.loadFFTwisdom()
+    xt = zeros(ComplexF64, length(grid.t), length(q.r))
+    FT = FFTW.plan_fft(xt, 1, flags=settings["fftw_flag"])
+    Eω = zeros(ComplexF64, length(grid.ω), length(q.k))
+    Eωk = q * Eω
+    doinputs_fs!(Eωk, grid, q, FT, inputs)
+    xo = Array{ComplexF64}(undef, length(grid.to), length(q.r))
+    FTo = FFTW.plan_fft(xo, 1, flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransRadial(grid, q, FTo, responses, densityfun, normfun)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eωk, transform, FT
+end
+
+function setup(grid::Grid.RealGrid, xygrid::Grid.FreeGrid,
+               densityfun, normfun, responses, inputs)
+    Utils.loadFFTwisdom()
+    x = xygrid.x
+    y = xygrid.y          
+    xr = Array{Float64}(undef, length(grid.t), length(y), length(x))
+    FT = FFTW.plan_rfft(xr, (1, 2, 3), flags=settings["fftw_flag"])
+    Eωk = zeros(ComplexF64, length(grid.ω), length(y), length(x))
+    doinputs_fs!(Eωk, grid, xygrid, FT, inputs)
+    xo = Array{Float64}(undef, length(grid.to), length(y), length(x))
+    FTo = FFTW.plan_rfft(xo, (1, 2, 3), flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransFree(grid, xygrid, FTo,
+                                       responses, densityfun, normfun)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eωk, transform, FT
+end
+
+function setup(grid::Grid.EnvGrid, xygrid::Grid.FreeGrid,
+               densityfun, normfun, responses, inputs)
+    Utils.loadFFTwisdom()
+    x = xygrid.x
+    y = xygrid.y          
+    xr = Array{ComplexF64}(undef, length(grid.t), length(y), length(x))
+    FT = FFTW.plan_fft(xr, (1, 2, 3), flags=settings["fftw_flag"])
+    Eωk = zeros(ComplexF64, length(grid.ω), length(y), length(x))
+    doinputs_fs!(Eωk, grid, xygrid, FT, inputs)
+    xo = Array{ComplexF64}(undef, length(grid.to), length(y), length(x))
+    FTo = FFTW.plan_fft(xo, (1, 2, 3), flags=settings["fftw_flag"])
+    transform = NonlinearRHS.TransFree(grid, xygrid, FTo,
+                                       responses, densityfun, normfun)
+    inv(FT) # create inverse FT plans now, so wisdom is saved
+    inv(FTo)
+    Utils.saveFFTwisdom()
+    Eωk, transform, FT
+end
+
+linoptype(l::AbstractArray) = "constant"
+linoptype(l) = "variable"
+
+gridtype(g::Grid.RealGrid) = "field-resolved"
+gridtype(g::Grid.EnvGrid) = "envelope"
+gridtype(g) = "unknown"
+
+simtype(g, t, l) = Dict("field" => gridtype(g),
+                        "transform" => string(t),
+                        "linop" => linoptype(l))
+
+function dumps(t, l)
+    io = IOBuffer()
+    dump(io, t)
+    tr = String(take!(io))
+    io = IOBuffer()
+    dump(io, l)
+    lo = String(take!(io))
+    Dict("transform" => tr, "linop" => lo)
+end
+
+function run(Eω, grid,
+             linop, transform, FT, output;
+             min_dz=0, max_dz=grid.zmax/2, init_dz=1e-4, z0=0.0,
+             rtol=1e-6, atol=1e-10, safety=0.9, norm=RK45.weaknorm,
+             status_period=1)
+
+    Et = FT \ Eω
+
+    function stepfun(Eω, z, dz, interpolant)
+        Eω .*= grid.ωwin
+        ldiv!(Et, FT, Eω)
+        Et .*= grid.twin
+        mul!(Eω, FT, Et)
+        output(Eω, z, dz, interpolant)
+    end
+
+    # check_cache does nothing except for HDF5Outputs
+    Eωc, zc, dzc = Output.check_cache(output, Eω, z0, init_dz)
+    if zc > z0
+        Logging.@info("Found cached propagation. Resuming...")
+        Eω, z0, init_dz = Eωc, zc, dzc
+    end
+
+    output(Grid.to_dict(grid), group="grid")
+    output(simtype(grid, transform, linop), group="simulation_type")
+    output(dumps(transform, linop), group="dumps")
+
+    RK45.solve_precon(
+        transform, linop, Eω, z0, init_dz, grid.zmax, stepfun=stepfun,
+        max_dt=max_dz, min_dt=min_dz,
+        rtol=rtol, atol=atol, safety=safety, norm=norm,
+        status_period=status_period)
+end
+
+end # module
