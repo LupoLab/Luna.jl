2b9847b3d7fab9916b1d9f3ce3e716a56e5553ba
Mohammed Sabbah <ms246@hw.ac.uk>
2020-09-22 09:35:28 +0100

    start

--- examples/basic_modeAvg.jl
+++ examples/basic_modeAvg.jl
@@ -1,13 +1,13 @@
 using Luna
 
-a = 13e-6
+a = 15e-6
 gas = :Ar
-pres = 5
-flength = 15e-2
+pres = 6
+flength = 0.225
 
 τfwhm = 30e-15
 λ0 = 800e-9
-energy = 1e-6
+energy = 2.5e-6
 
 grid = Grid.RealGrid(flength, λ0, (160e-9, 3000e-9), 1e-12)
 
@@ -36,7 +36,7 @@ inputs = Fields.GaussField(λ0=λ0, τfwhm=τfwhm, energy=energy)
 Eω, transform, FT = Luna.setup(grid, densityfun, responses, inputs, βfun!, aeff)
 
 statsfun = Stats.default(grid, Eω, m, linop, transform; gas=gas, windows=((150e-9, 300e-9),))
-output = Output.HDF5Output("test.h5", 0, grid.zmax, 201, statsfun)
+output = Output.HDF5Output("test7.h5", 0, grid.zmax, 201, statsfun)
 
 Luna.run(Eω, grid, linop, transform, FT, output)
 
