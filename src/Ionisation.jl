7e166f8117d158e08ad30e6e2bfb54cc7e9dd07a
cbrahms <c.brahms@hw.ac.uk>
2021-04-05 15:24:08 +0100

    ionisation fraction function

--- src/Ionisation.jl
+++ src/Ionisation.jl
@@ -152,6 +152,14 @@ function keldysh(material, λ, E)
     ω0_au*sqrt(2Ip_au)/E_au
 end
 
+function ionfrac(rate, E, δt)
+    r = similar(E)
+    frac = similar(E)
+    rate(r, E)
+    Maths.cumtrapz!(frac, r, δt)
+    @. frac = 1 - exp(-frac)
+end
+
 function makePPTaccel(E, rate)
     cspl = Maths.CSpline(E, log.(rate); bounds_error=true)
     Emin = minimum(E)
