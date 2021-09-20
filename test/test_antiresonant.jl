aa66927b6fc0e44d2a1186d747dc1f92e20b001c
cbrahms <c.brahms@hw.ac.uk>
2021-09-20 12:43:00 +0100

    fix antiresonant test

--- test/test_antiresonant.jl
+++ test/test_antiresonant.jl
@@ -19,11 +19,11 @@ import Luna.PhysData: wlfreq
     ω = wlfreq.(λ)
     @test all(Modes.neff.(arm, ω) .== Modes.neff.(arm2, ω))
 
-    @test Modes.neff(arm, 2.5e15) == 0.9999193518567425 + 1.87925966056515e-6im
+    @test Modes.neff(arm, 2.5e15) ≈ 0.9999193518567425 + 1.87925966056515e-6im
     arm = Antiresonant.ZeisbergerMode(a, :Air, 0, (ω; z) -> 1.45; wallthickness=w, loss=false)
     @test Modes.neff(arm, 2.5e15) == 0.9999193518567425
     arm = Antiresonant.ZeisbergerMode(a, :Air, 0, (ω; z) -> 1.45; wallthickness=w, loss=0.5)
-    @test Modes.neff(arm, 2.5e15) == 0.9999193518567425 + 0.5*1.87925966056515e-6im
+    @test Modes.neff(arm, 2.5e15) ≈ 0.9999193518567425 + 0.5*1.87925966056515e-6im
     @test_throws ArgumentError Antiresonant.ZeisbergerMode(a, :Air, 0; wallthickness=w, loss=0.5im)
 end
 
