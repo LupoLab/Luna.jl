7d5760ed8e9808f80f07430736540ca732f5ef3e
John Travers <jtravs@gmail.com>
2022-08-23 22:03:04 +0100

    improve gnsle sol example

--- examples/simple_interface/gnlse_sol.jl
+++ examples/simple_interface/gnlse_sol.jl
@@ -5,18 +5,20 @@ using Luna
 γ = 0.1
 β2 = -1e-26
 N = 4.0
-τ0 = 28e-15
+τ0 = 280e-15
+τfwhm = (2*log(1 + sqrt(2)))*τ0
 fr = 0.18
 P0 = N^2*abs(β2)/((1-fr)*γ*τ0^2)
-flength = pi/2*τ0^2/abs(β2)
+flength = pi*τ0^2/abs(β2)
 βs =  [0.0, 0.0, β2]
 
 λ0 = 835e-9
 λlims = [450e-9, 8000e-9]
-trange = 2e-12
+trange = 4e-12
 
-output = prop_gnlse(γ, flength, βs; λ0, τfwhm=1.763*τ0, power=P0, pulseshape=:sech, λlims, trange, raman=false, shock=false, fr, shotnoise=false)
+output = prop_gnlse(γ, flength, βs; λ0, τfwhm, power=P0, pulseshape=:sech, λlims, trange,
+                    raman=false, shock=false, fr, shotnoise=false)
 
 ##
 Plotting.pygui(true)
-Plotting.prop_2D(output, :ω, dBmin=-40.0,  λrange=(500e-9,2500e-9), trange=(-50e-15, 50e-15), oversampling=1)
+Plotting.prop_2D(output, :ω, dBmin=-100.0,  λrange=(720e-9,1000e-9), trange=(-300e-15, 300e-15), oversampling=1)
