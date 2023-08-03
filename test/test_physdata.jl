e8a4aaf0db19a3c51230c1cfad1392e4061f3a03
chrisbrahms <c.brahms@hw.ac.uk>
2023-08-03 20:20:56 +0100

    fix tests

--- test/test_physdata.jl
+++ test/test_physdata.jl
@@ -3,12 +3,12 @@ import Luna: PhysData
 
 @testset "All" begin
 @testset "Exceptions" begin
-    @test_throws DomainError PhysData.ref_index(:Hello, 800e-9)
+    @test_throws DomainError PhysData.ref_index(:HeBllo, 800e-9)
 end
 
 @testset "refractive indices" begin
-    @test PhysData.ref_index(:He, 800e-9) ≈ 1.000031950041203
-    @test PhysData.ref_index(:He, 800e-9, 10) ≈ 1.0003180633169397
+    @test PhysData.ref_index(:HeB, 800e-9) ≈ 1.000031950041203
+    @test PhysData.ref_index(:HeB, 800e-9, 10) ≈ 1.0003180633169397
     @test PhysData.ref_index(:SiO2, 800e-9, lookup=false) ≈ 1.4533172548587419
     @test PhysData.ref_index(:SiO2, 400e-9, lookup=false) ≈ 1.4701161185594052
     @test PhysData.ref_index(:SiO2, PhysData.eV_to_m(6)) ≈ 1.543
@@ -20,13 +20,13 @@ end
 
 @testset "Function equivalence" begin
     @test PhysData.ref_index_fun(:SiO2)(800e-9) == PhysData.ref_index(:SiO2, 800e-9)
-    @test PhysData.ref_index_fun(:He)(800e-9) == PhysData.ref_index(:He, 800e-9)
+    @test PhysData.ref_index_fun(:HeB)(800e-9) == PhysData.ref_index(:HeB, 800e-9)
 end
 
 @testset "Dispersion" begin
     @test PhysData.dispersion(2, :SiO2, 800e-9, lookup=false) ≈ 3.61619983e-26
-    @test isapprox(PhysData.dispersion(2, :He, 800e-9), 9.373942337550116e-31, rtol=1e-5)
-    @test isapprox(PhysData.dispersion(2, :He, 800e-9, 10), 9.33043805928079e-30, rtol=1e-5)
+    @test isapprox(PhysData.dispersion(2, :HeB, 800e-9), 9.373942337550116e-31, rtol=1e-5)
+    @test isapprox(PhysData.dispersion(2, :HeB, 800e-9, 10), 9.33043805928079e-30, rtol=1e-5)
 end
 
 @testset "glasses" begin
@@ -48,12 +48,12 @@ end
 end
 
 @testset "Nonlinear coefficients" begin
-    @test_broken PhysData.χ3(:He, 1) ≈ 1.2617371645226101e-27
+    @test_broken PhysData.χ3(:HeB, 1) ≈ 1.2617371645226101e-27
     @test PhysData.χ3(:Ar, 1) ≈ 2.964158749949189e-26
-    @test_broken PhysData.n2(:He, 1) ≈ 3.5647819877255427e-25
-    @test PhysData.n2(:He, 2) ≈ 7.125642138007481e-25
-    @test_broken PhysData.n2.(:He, [1, 2]) ≈ [3.5647819877255427e-25, 7.125642138007481e-25]
-    @test_broken PhysData.n2.([:He, :Ne], 1) ≈ [3.5647819877255427e-25, 6.416061508801999e-25]
+    @test_broken PhysData.n2(:HeB, 1) ≈ 3.5647819877255427e-25
+    @test PhysData.n2(:HeB, 2) ≈ 7.125642138007481e-25
+    @test_broken PhysData.n2.(:HeB, [1, 2]) ≈ [3.5647819877255427e-25, 7.125642138007481e-25]
+    @test_broken PhysData.n2.([:HeB, :Ne], 1) ≈ [3.5647819877255427e-25, 6.416061508801999e-25]
     for gas in PhysData.gas[2:end] # Don't have γ3 for Air
         @test isreal(PhysData.n2(gas, 1))
     end
@@ -67,11 +67,11 @@ end
     @test isapprox(PhysData.density(:Ar, 2.0, 294.0), 4.933761614600933e25, rtol=4e-13)
     @test isapprox(PhysData.density(:Ar, 40.0, 294.0), 1.0101579129300146e27, rtol=2e-11)
     @test isapprox(PhysData.density(:Ar, 400.0, 294.0), 9.270757850984163e27, rtol=3e-10)
-    @test_broken isapprox(PhysData.density(:He, 0.002, 294.0), 4.927180563885407e22, rtol=7e-16)
-    @test_broken isapprox(PhysData.density(:He, 0.02, 294.0), 4.927137517453137e23, rtol=2e-16)
-    @test_broken isapprox(PhysData.density(:He, 2.0, 294.0), 4.9224080868066745e25, rtol=2e-13)
-    @test_broken isapprox(PhysData.density(:He, 40.0, 294.0), 9.66755645602771e26, rtol=6e-16)
-    @test_broken isapprox(PhysData.density(:He, 400.0, 294.0), 8.309132317978155e27, rtol=1e-13)
+    @test_broken isapprox(PhysData.density(:HeB, 0.002, 294.0), 4.927180563885407e22, rtol=7e-16)
+    @test_broken isapprox(PhysData.density(:HeB, 0.02, 294.0), 4.927137517453137e23, rtol=2e-16)
+    @test_broken isapprox(PhysData.density(:HeB, 2.0, 294.0), 4.9224080868066745e25, rtol=2e-13)
+    @test_broken isapprox(PhysData.density(:HeB, 40.0, 294.0), 9.66755645602771e26, rtol=6e-16)
+    @test_broken isapprox(PhysData.density(:HeB, 400.0, 294.0), 8.309132317978155e27, rtol=1e-13)
     @test isapprox(PhysData.density(:Xe, 0.002, 294.0), 4.927238737907382e22, rtol=7e-16)
     @test isapprox(PhysData.density(:Xe, 0.02, 294.0), 4.92771934762324e23, rtol=2e-16)
     @test isapprox(PhysData.density(:Xe, 2.0, 294.0), 4.981603290885485e25, rtol=9e-16)
@@ -89,9 +89,9 @@ end
     @test isapprox(PhysData.density(:H2, 40.0, 294.0), 9.624525613497868e26, rtol=6e-16)
     @test isapprox(PhysData.density(:H2, 400.0, 294.0), 7.840662987579036e27, rtol=6e-16)
     # temperature range
-    @test_broken isapprox(PhysData.density(:He, 10.0, 10.0), 9.218927214187151e27, rtol=6e-16)
-    @test_broken isapprox(PhysData.density(:He, 10.0, 100.0), 7.142310177179197e26, rtol=6e-16)
-    @test_broken isapprox(PhysData.density(:He, 10.0, 1000.0), 7.234660811823096e25, rtol=6e-13)
+    @test_broken isapprox(PhysData.density(:HeB, 10.0, 10.0), 9.218927214187151e27, rtol=6e-16)
+    @test_broken isapprox(PhysData.density(:HeB, 10.0, 100.0), 7.142310177179197e26, rtol=6e-16)
+    @test_broken isapprox(PhysData.density(:HeB, 10.0, 1000.0), 7.234660811823096e25, rtol=6e-13)
     @test isapprox(PhysData.density(:Xe, 10.0, 170.0), 1.336261228046876e28, rtol=6e-16)
     @test isapprox(PhysData.density(:Xe, 10.0, 700.0), 1.0361035050644844e26, rtol=6e-13)
     @test isapprox(PhysData.density(:N2, 10.0, 100.0), 1.4849593021049305e28, rtol=6e-7)
