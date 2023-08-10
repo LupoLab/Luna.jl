acdcd6d57b7af0253cf11a6c5a432088f63d4712
chrisbrahms <c.brahms@hw.ac.uk>
2023-08-10 14:16:11 +0100

    fix cmap_white

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -34,10 +34,10 @@ function cmap_white(cmap; N=2^12, n=8)
     vals = collect(range(0, 1, length=n))
     vals_i = collect(range(0, 1, length=N))
     cm = ColorMap(cmap)
-    clist = cm(vals)
+    clist = pyconvert(Array, cm(vals))
     clist[1, :] = [1, 1, 1, 1]
-    clist_i = Array{Float64}(undef, (N, 4))
-    for ii in 1:4
+    clist_i = Array{Float64}(undef, (N, 3))
+    for ii in 1:3
         clist_i[:, ii] .= Maths.BSpline(vals, clist[:, ii]).(vals_i)
     end
     ColorMap(clist_i)
