25bb375ed25a725faffa25e3e19e7b01b01621af
chrisbrahms <c.brahms@hw.ac.uk>
2025-08-04 20:42:53 +0100

    test for PPT options

--- test/test_ionisation.jl
+++ test/test_ionisation.jl
@@ -1,7 +1,7 @@
 import Test: @test, @testset
-import Luna: Ionisation, Maths, PhysData, Tools
+using Luna
 import NumericalIntegration: integrate, SimpsonEven
-import Luna.Modes: hquadrature
+import Logging: with_logger, NullLogger
 
 @test Ionisation.ionrate_ADK(:He, 1e10) ≈ 1.2416371415312408e-18
 @test Ionisation.ionrate_ADK(:He, 2e10) ≈ 1.0772390893742478
@@ -12,8 +12,8 @@ import Luna.Modes: hquadrature
 E = collect(range(1e9, 1e11; length=32))
 @test Ionisation.ionrate_ADK(:He, E) == Ionisation.ionrate_ADK(:He, -E)
 
-@test isapprox(Ionisation.ionrate_PPT(:He, 800e-9, 1e10), 2*1.40432138471583e-5, rtol=1e-3)
-@test isapprox(Ionisation.ionrate_PPT(:He, 800e-9, 1.3e10), 2*0.04517809797503506, rtol=1e-3)
+@test isapprox(Ionisation.ionrate_PPT(:He, 800e-9, 1e10; dipole_corr=false), 2*1.40432138471583e-5, rtol=1e-3)
+@test isapprox(Ionisation.ionrate_PPT(:He, 800e-9, 1.3e10; dipole_corr=false), 2*0.04517809797503506, rtol=1e-3)
 
 Emin = 1e9
 Emax = 1e11
@@ -53,12 +53,8 @@ ratefun!(outneg, -E)
         Ionisation.barrier_suppression(
             PhysData.ionisation_potential(gas),
             1))
-    isy = 10 .^ collect(16:0.01:log10(bsi))
+    isy = 10 .^ collect(16:0.1:log10(bsi))
     E0 = Tools.intensity_to_field.(isy)
-
-    Ip_au = PhysData.ionisation_potential(gas; unit=:atomic)
-    F0_au = (2Ip_au)^(3/2)
-    F0 = F0_au*PhysData.au_Efield
 
     t = collect(range(0, 2π; length=2^12))
     Et = sin.(t)
@@ -71,7 +67,59 @@ ratefun!(outneg, -E)
         adk_avg[idx] = 1/2π * integrate(t, out, SimpsonEven())
     end
 
-    adk = Ionisation.ionrate_ADK.(gas, E0)
     adk_avg_kw = Ionisation.ionrate_ADK.(gas, E0; cycle_average=true)
     @test all(isapprox.(adk_avg, adk_avg_kw; rtol=0.05))
+end
+
+##
+# Check that PPT_options are indeed passed through properly
+@testset for gas in (:He, :Ne, :Ar, :Kr, :Xe)
+    # select all non-default options--each of these will make a difference to the interpolant
+    PPT_options = Dict{Symbol, Any}(
+        :stark_shift => false,
+        :dipole_corr => false,
+        :sum_tol => 1e-4,
+        :sum_integral => true,
+        :msum => false,
+        :occupancy => 4,
+    )
+
+    λ0 = 800e-9
+    Eω, grid, linop, transform, FT, output = with_logger(NullLogger()) do
+        Interface.prop_capillary_args(100e-6, 1, gas, 1;
+                                      λ0, τfwhm=10e-15, energy=1e-6,
+                                      λlims=(200e-9, 4e-6), trange=0.5e-12,
+                                      PPT_options)
+    end
+
+    plasma = transform.resp[2]
+    ir = plasma.ratefunc
+
+    ir2 = Ionisation.ionrate_fun!_PPTaccel(gas, λ0; PPT_options...)
+
+    @test ir2.cspl.x == ir.cspl.x
+    @test ir2.cspl.y == ir.cspl.y
+
+    # now same again with default options
+    Eω, grid, linop, transform, FT, output = with_logger(NullLogger()) do
+        Interface.prop_capillary_args(100e-6, 1, gas, 1;
+                                      λ0, τfwhm=10e-15, energy=1e-6,
+                                      λlims=(200e-9, 4e-6), trange=0.5e-12)
+    end
+
+    plasma = transform.resp[2]
+    ir = plasma.ratefunc
+
+    PPT_options = Dict{Symbol, Any}(
+        :stark_shift => true,
+        :dipole_corr => true,
+        :sum_tol => 1e-6,
+        :sum_integral => false,
+        :msum => true,
+        :occupancy => 2,
+    )
+    ir2 = Ionisation.ionrate_fun!_PPTaccel(gas, λ0; PPT_options...)
+
+    @test ir2.cspl.x == ir.cspl.x
+    @test ir2.cspl.y == ir.cspl.y
 end
