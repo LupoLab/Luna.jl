15b43695dff0dc5cd5d7777e644da41247d0f330
chrisbrahms <c.brahms@hw.ac.uk>
2025-08-27 09:23:29 +0100

    stdbuf doesn't work

--- src/Scans.jl
+++ src/Scans.jl
@@ -448,7 +448,7 @@ function runscan(f, scan::Scan{SlurmExec})
         "#SBATCH -e %x_%a.stderr",
         "#SBATCH --array=1-$cores",
         "#SBATCH --chdir $dir",
-        "stdbuf -o0 -e0 julia $(basename(script)) --queue"
+        "julia $(basename(script)) --queue"
     ]
     subfile = joinpath(dir, "$name.sh")
     @info "Writing job file to $subfile..."
